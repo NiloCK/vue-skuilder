6500258217a7b671afbe92d910d942f6549461b3
nilock <colinkennedy@hotmail.com>
2018-03-27 23:28:54 -0600

    Rearrange type definition location

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,92 +2,39 @@ import pouch from 'pouchdb-browser';
 import * as PouchDBFind from 'pouchdb-find';
 import PouchDBAuth from 'pouchdb-authentication';
 import { Answer } from '@/courses/base/Course';
+import { CourseListData } from '@/db/types';
 
 pouch.plugin(PouchDBAuth);
 pouch.plugin(PouchDBFind);
 
 const databaseName = 'record';
 const remote: PouchDB.Database = new pouch(
-    'http://localhost:5984/math/',
+    'http://localhost:5984/skuilder/',
     {
         skip_setup: true
     }
 );
 const local: PouchDB.Database = new pouch('local');
 
-interface PouchData extends PouchDB.Core.IdMeta, PouchDB.Core.GetMeta {
-    data: string[];
-}
-
-enum DocType {
-    DISPLAYABLE_DATA,
-    CARD,
-    DATASHAPE,
-    VIEW,
-    PEDAGOGY
-}
-
 /**
- * Interface for all data on course content and pedagogy stored
- * in the c/pouch database.
+ *
  */
-interface SkuilderCourseData extends PouchDB.Core.IdMeta, PouchDB.Core.GetMeta {
-    course: string;
-    docType: DocType;
-}
-interface CardData extends SkuilderCourseData {
-    // DocType.CARD
-    id_displayable_data: PouchDB.Core.DocumentId;
-    id_view: PouchDB.Core.DocumentId;
-}
-
-/**
- * The name of a Viewable component in a skuilder course
- */
-type VueComponentName = string;
-interface ViewData extends SkuilderCourseData {
-    // DocType.VIEW
-    id_datashape: PouchDB.Core.DocumentId;
-    name: VueComponentName;
-}
-
-/**
- * The data used to hydrate viewable components (questions, info, etc)
- */
-interface DisplayableData extends SkuilderCourseData {
-    // DocType.DISPLAYABLE_DATA
-    id_datashape: PouchDB.Core.DocumentId;
-    data: Field[];
-}
-
-interface Field {
-    data: any;
-    name: string;
-}
-
-/**
- * The name of a defined interface for ctor args of a question type or viewable type
- */
-type ViewableTypeCtor = string;
-interface DataShapeData extends SkuilderCourseData {
-    // DocType.DATASHAPE
-    name: ViewableTypeCtor;
-    fields: FieldDefinition[];
-}
-
-interface FieldDefinition {
-    name: string;
-    type: FieldType;
-}
-
-enum FieldType {
-    STRING = 'string',
-    NUMBER = 'number',
-    INT = 'int'
-}
-
-export function testDataRetrieval(): Promise<PouchData> {
-    return remote.get<PouchData>('testData');
+export async function getCourseList(): Promise<CourseListData> {
+    // try {
+    //     const ret = await remote.get<CourseListData>('courses');
+    //     return ret;
+    // } catch {
+    //     // tslint:disable-next-line:no-console
+    //     console.log('hi');
+    // }
+    // // const ret = await remote.get<CourseListData>('bourses');
+    // // if (ret.courses) {
+    // //     return ret;
+    // // } else {
+    // //     // tslint:disable-next-line:no-console
+    // //     console.log('hello');
+    // // }
+    return remote.get<CourseListData>('courses');
 }
 
 export function getCourseDatabase(courseName: string): PouchDB.Database {
@@ -105,13 +52,4 @@ export function getCourseQuestions(courseName: string) {
         fields: ['type'],
         selector: { name: 'viewList' }
     });
-}
-
-export interface QuestionRecord {
-    courseID: string;
-    cardID: string;
-    userAnswer: Answer;
-    isCorrect: boolean;
-    attempts: number;
-    time: number;
 }
---
+++ src/db/types.ts
@@ -0,0 +1,85 @@
+import { Answer } from '@/courses/base/Course';
+
+export enum DocType {
+    DISPLAYABLE_DATA,
+    CARD,
+    DATASHAPE,
+    VIEW,
+    PEDAGOGY
+}
+
+/**
+ * Interface for all data on course content and pedagogy stored
+ * in the c/pouch database.
+ */
+export interface SkuilderCourseData extends
+        PouchDB.Core.IdMeta,
+        PouchDB.Core.GetMeta,
+        PouchDB.Core.Response {
+    course: string;
+    docType: DocType;
+}
+export interface CardData extends SkuilderCourseData {
+    // DocType.CARD
+    id_displayable_data: PouchDB.Core.DocumentId;
+    id_view: PouchDB.Core.DocumentId;
+}
+
+/** A list of populated courses in the DB */
+export interface CourseListData extends PouchDB.Core.Response {
+    courses: string[];
+}
+
+/**
+ * The name of a Viewable component in a skuilder course
+ */
+export type VueComponentName = string;
+export interface ViewData extends SkuilderCourseData {
+    // DocType.VIEW
+    id_datashape: PouchDB.Core.DocumentId;
+    name: VueComponentName;
+}
+
+/**
+ * The data used to hydrate viewable components (questions, info, etc)
+ */
+export interface DisplayableData extends SkuilderCourseData {
+    // DocType.DISPLAYABLE_DATA
+    id_datashape: PouchDB.Core.DocumentId;
+    data: Field[];
+}
+
+export interface Field {
+    data: any;
+    name: string;
+}
+
+/**
+ * The name of a defined interface for ctor args of a question type or viewable type
+ */
+export type ViewableTypeCtor = string;
+export interface DataShapeData extends SkuilderCourseData {
+    // DocType.DATASHAPE
+    name: ViewableTypeCtor;
+    fields: FieldDefinition[];
+}
+
+export interface FieldDefinition {
+    name: string;
+    type: FieldType;
+}
+
+export enum FieldType {
+    STRING = 'string',
+    NUMBER = 'number',
+    INT = 'int'
+}
+
+export interface QuestionRecord {
+    course: string;
+    cardID: string;
+    userAnswer: Answer;
+    isCorrect: boolean;
+    attempts: number;
+    time: number;
+}
