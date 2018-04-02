3c52ee898628b3a79d0bbc97a857f573c085adb4
nilock <colinkennedy@hotmail.com>
2018-04-01 20:29:26 -0600

    Add a division question type

---
+++ src/courses/math/questions/division/horizontal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      {{question.data.a * question.data.b}} / {{question.data.b}} = 
+       <input type="text" v-model="answer" @keyup.enter="submit" />
+  </div>
+</template>
+
+// todo replace '/' in the question with the correct 'division' symbol
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Viewable from '@/courses/base/Viewable.vue';
+import { SingleDigitDivisionQuestion } from './index';
+
+@Component
+export default class MultiplicationHorizontal extends Viewable {
+  public answer: string = '';
+  @Prop()
+  public question: SingleDigitDivisionQuestion = new SingleDigitDivisionQuestion();
+
+  public submit() {
+    alert(this.question.isCorrect(parseInt(this.answer, 10)));
+  }
+}
+</script>
---
+++ src/courses/math/questions/division/index.ts
@@ -0,0 +1,47 @@
+import { Question, Answer } from '@/base-course/Course';
+import { randomInt } from '@/courses/math/utility';
+import { DataShapeData, FieldType, FieldDefinition } from '@/db/types';
+
+interface SingleDigitDivisionQuestionProps {
+    a: number;
+    b: number;
+}
+
+const fields: FieldDefinition[] = [
+    {
+        name: 'a',
+        type: FieldType.INT
+    },
+    {
+        name: 'b',
+        type: FieldType.INT
+    }
+];
+
+export default fields;
+
+export class SingleDigitDivisionQuestion extends Question {
+    public static dataShape = {
+        name: 'SingleDigitDivisionQuestion',
+        fields
+    };
+    public data: SingleDigitDivisionQuestionProps;
+
+    /**
+     * @param data a and b are seed props that will pop a question of
+     * the form [(a*b) / b = ___]. So, b must be non-zero.
+     */
+    constructor(data?: SingleDigitDivisionQuestionProps) {
+        super();
+        this.data = data ? data : {
+            a: randomInt(0, 10),
+            b: randomInt(1, 10)
+        };
+    }
+
+    public isCorrect(answer: Answer) {
+        const { a, b } = this.data;
+
+        return a * b === answer;
+    }
+}
