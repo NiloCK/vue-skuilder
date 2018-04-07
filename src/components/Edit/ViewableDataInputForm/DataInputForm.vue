219fabe30c8a19f72a271e9f882a4e78c8b37c23
nilock <colinkennedy@hotmail.com>
2018-04-07 11:16:35 -0600

    Add skeleton of DataInputForm

---
+++ src/components/Edit/ViewableDataInputForm/DataInputForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <form action="">
+      <div v-for="field in fields" :key="field.name" :value="field">
+          <FieldInput field="field" />
+      </div>
+  </form>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+import { FieldDefinition } from '@/base-course/Course';
+
+@Component({})
+export default class DataInputForm extends Vue {
+  @Prop() public fields: FieldDefinition[] = [];
+
+}
+</script>
+
