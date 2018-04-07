89a0f2ac04691bd4ab73415a17cfcf1fcfc34ae7
nilock <colinkennedy@hotmail.com>
2018-04-07 13:08:43 -0600

    Add type validators for text input fields

---
+++ src/components/Edit/ViewableDataInputForm/FieldInputs/typeValidators.ts
@@ -0,0 +1,47 @@
+import { ValidationResult, Status } from '@/base-course/Course';
+import { isNumber } from 'util';
+
+const okResult: ValidationResult = {
+    status: Status.ok,
+    msg: ''
+};
+
+export function numberValidator(value: string): ValidationResult {
+    const errorResult: ValidationResult = {
+        status: Status.error,
+        msg: 'This input must be a number'
+    };
+
+    if (isNumeric(value)) {
+        return okResult;
+    } else {
+        return errorResult;
+    }
+}
+
+export function integerValidator(value: string): ValidationResult {
+    const errorResult: ValidationResult = {
+        status: Status.error,
+        msg: 'This input must be an integer (..., -2, -1, 0, 1, 2, ...).'
+    };
+
+    if (numberValidator(value).status === Status.error) {
+        return errorResult;
+    }
+
+    if (isInteger(value)) {
+        return okResult;
+    } else {
+        return errorResult;
+    }
+}
+
+function isNumeric(value: any): boolean {
+    // pilfered from Angular and assumed to be correctish:
+    // https://github.com/angular/angular/blob/4.3.x/packages/common/src/pipes/number_pipe.ts#L172
+    return !isNaN(value - parseFloat(value));
+}
+
+function isInteger(value: string) {
+    return /^[\+,\-]?\s?\d+$/.test(value);
+}
