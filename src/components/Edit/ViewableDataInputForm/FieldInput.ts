ea57681cc17726f7551bf83a9564071793c9e6ec
nilock <colinkennedy@hotmail.com>
2018-04-09 17:03:15 -0600

    Rename file

--- src/components/Edit/ViewableDataInputForm/FieldInput.ts
+++ src/components/Edit/ViewableDataInputForm/FieldInput.ts
@@ -1,5 +1,10 @@
 import { ValidatingFunction } from '@/base-course/Interfaces/ValidatingFunction';
+import Vue from 'vue';
 
 export interface ValidatedInput {
     getValidators: () => ValidatingFunction[];
+}
+
+export abstract class FieldInput extends Vue {
+    public abstract getValidators(): ValidatingFunction[];
 }
