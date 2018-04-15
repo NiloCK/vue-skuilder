4a4df8dfef9c0579bcf460fc07ba12de190f0223
nilock <colinkennedy@hotmail.com>
2018-04-15 16:40:19 -0600

    Add list of Views as a property of DataShape

--- src/base-course/Interfaces/DataShape.ts
+++ src/base-course/Interfaces/DataShape.ts
@@ -1,7 +1,10 @@
-import { NoteCtor } from '@/db/types';
+import Vue from 'vue';
+import {VueConstructor} from 'vue';
+import { NoteCtor, VueComponentName } from '@/db/types';
 import { FieldDefinition } from '@/base-course/Interfaces/FieldDefinition';
 
 export interface DataShape {
     name: NoteCtor;
     fields: FieldDefinition[];
+    views: Array<VueConstructor<Vue>>;
 }
