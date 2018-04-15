e7f8fc90fac22589fea8f68067fad49c472934ed
nilock <colinkennedy@hotmail.com>
2018-04-15 16:55:54 -0600

    Add function to generate map of question Views
    (given a map of courses)

--- src/courses/index.ts
+++ src/courses/index.ts
@@ -1,8 +1,26 @@
 import math from './math';
 import { Course } from '@/base-course/Course';
 
-interface CourseList {
+export interface CourseList {
     [index: string]: Course;
+}
+
+export function getViews(courses: CourseList) {
+    const ret: any = {};
+    Object.keys(courses).forEach( (course) => {
+        alert(`Course: ${JSON.stringify(course)}`);
+        courses[course].viewableTypes.forEach( (type) => {
+            alert(`dataShape type: ${type.name}`);
+            type.dataShape.views.forEach( (view) => {
+                if (view) {
+                    alert(JSON.stringify(view));
+                    ret[view.name] = view.name;
+                }
+            });
+        });
+    });
+
+    return ret;
 }
 
 const courseList: CourseList = {
