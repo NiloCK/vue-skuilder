95c4ca8e21bad0020b8413f56859a489bf14705c
Colin Kennedy <colink@mun.ca>
2018-04-16 15:43:50 -0600

    Add timer, getTime function to Viewable

--- src/base-course/Viewable.vue
+++ src/base-course/Viewable.vue
@@ -5,12 +5,24 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import { DisplayableData } from '@/db/types';
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { DisplayableData } from "@/db/types";
+import moment from "moment";
 
 @Component
 export default class Viewable extends Vue {
   @Prop() public data: any;
+  protected startTime: moment.Moment;
+
+  created() {
+    this.startTime = moment();
+  }
+  /** 
+   * Returns the time in MS since the element was created
+   */
+  getTime(): number {
+    return moment().diff(this.startTime, "milliseconds");
+  }
 }
 </script>
 
