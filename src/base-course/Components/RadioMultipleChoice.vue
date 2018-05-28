46ef6855831b56688f84824d118488a24635b953
Colin Kennedy <colink@mun.ca>
2018-05-27 18:31:57 -0600

    Add skeleton for radio multiple choice component

---
+++ src/base-course/Components/RadioMultipleChoice.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <select name="" id="" type="radio">
+          <option value="a">a</option>
+          <option value="b">b</option>
+      </select>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator';
+
+@Component({
+
+})
+export default class RadioSelect extends Vue {
+    @Prop() public choiceList: string[] = ["hi", "bye"];
+}
+</script>
+
+<style scoped>
+
+</style>
