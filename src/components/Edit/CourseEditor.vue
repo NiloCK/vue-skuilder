485eed0242b2f840d7345d34c95869c42315d3de
nilock <colinkennedy@hotmail.com>
2018-06-25 22:49:48 -0600

    #17 Make Content-edit / Component-reg toggle

--- src/components/Edit/CourseEditor.vue
+++ src/components/Edit/CourseEditor.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="courseEditor" v-if="course">
-      <div v-if='loading'>
-        <Spinner />
-        Loading course information from database...
-      </div>
-      <div v-else>
+    <div v-if='loading'>
+      <Spinner /> Loading course information from database...
+    </div>
+    <div v-else>
+      <button @click='toggleComponent'>Toggle Content Editing / Component Registration</button>
+      <div v-if='editingMode'>
+
         <div>
-            Thereare {{ registeredDataShapes.length }} registered data shapes in the course.
+          {{`There ${registeredDataShapes.length !== 1 ? 'are' : 'is'} ${registeredDataShapes.length} registered data shape${registeredDataShapes.length === 1 ? '' : 's'} in the course.`}}
         </div>
         Which data type are you adding?
         <select v-model="selectedShape">
@@ -14,13 +16,15 @@
             {{shape.name}}
           </option>
         </select>
-        
-        <DataInputForm
+
+        <DataInputForm 
           v-if="selectedShape.name"
           v-bind:dataShape="selectedShape"
-          v-bind:course="course"
-        />
+          v-bind:course="course" />
       </div>
+      <ComponentRegistration v-else />
+
+    </div>
   </div>
 </template>
 
@@ -36,29 +40,32 @@ import { getDataShape } from '@/courses';
 import { getDataShapes, getDoc } from '@/db';
 import DataInputForm from './ViewableDataInputForm/DataInputForm.vue';
 import { DataShapeData } from '@/db/types';
+import ComponentRegistration from '@/components/Edit/ComponentRegistration/ComponentRegistration.vue';
 import _ from 'lodash';
 
 @Component({
   components: {
     DataInputForm,
-    Spinner
+    Spinner,
+    ComponentRegistration
   }
 })
 export default class CourseEditor extends Vue {
   @Prop() public course: string;
   public registeredDataShapes: DataShape[] = [];
   public dataShapes: DataShape[] = [];
-  public selectedShape: DataShape = {name: '', fields: [], views: []};
+  public selectedShape: DataShape = { name: '', fields: [], views: [] };
   private loading: boolean = true;
+  private editingMode: boolean = true;
 
   public created() {
-    Courses[this.course].viewableTypes.forEach( (type) => {
+    Courses[this.course].viewableTypes.forEach((type) => {
       this.dataShapes.push(type.dataShape);
     });
 
-    getDataShapes(this.course).then( (results) => {
-      results.docs.forEach( (doc) => {
-        getDoc<DataShapeData>(doc._id).then( (dataShapeDoc) => {
+    getDataShapes(this.course).then((results) => {
+      results.docs.forEach((doc) => {
+        getDoc<DataShapeData>(doc._id).then((dataShapeDoc) => {
           const shape = getDataShape(Courses, dataShapeDoc._id);
           if (shape) {
             this.registeredDataShapes.push(shape.dataShape);
@@ -66,10 +73,13 @@ export default class CourseEditor extends Vue {
           // alert(_.difference(this.dataShapes, this.registeredDataShapes));
         });
       });
-    }).then( () => {
+    }).then(() => {
       this.loading = false;
     });
+  }
 
+  private toggleComponent() {
+    this.editingMode = !this.editingMode;
   }
 }
 </script>
