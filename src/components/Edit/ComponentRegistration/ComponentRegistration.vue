3077ad752ba18b866920a7c4bd6c867723d90f9e
nilock <colinkennedy@hotmail.com>
2018-07-16 13:49:22 -0600

    #17 Enable Question registration

--- src/components/Edit/ComponentRegistration/ComponentRegistration.vue
+++ src/components/Edit/ComponentRegistration/ComponentRegistration.vue
@@ -14,6 +14,14 @@
        </ul>
       </li>  
     </ul>
+
+    <h3>Questions</h3>
+    <ul>
+      <li v-for="question in questions" :key="question.name">
+        {{ question.name }} - {{ question.registered }}
+        <button @click="registerQuestion(question.name)">Register</button>
+      </li>
+    </ul>
       
   </div>
 </template>
@@ -24,12 +32,20 @@ import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import { DataShape } from '@/base-course/Interfaces/DataShape';
 import Courses, { NameSpacer } from '@/courses';
-import { getDataShapes, getDoc, putDataShape } from '@/db';
+import { getDataShapes, getDoc, putDataShape, putQuestionType } from '@/db';
 import * as _ from 'lodash';
+import { Question } from '@/base-course/Course';
+import { QuestionData, QuestionRecord } from '@/db/types';
 
 interface DataShapeRegistrationStatus {
   name: string;
   dataShape: DataShape;
+  registered: boolean;
+}
+
+interface QuestionRegistrationStatus {
+  name: string;
+  question: typeof Question;
   registered: boolean;
 }
 
@@ -40,6 +56,7 @@ interface DataShapeRegistrationStatus {
 export default class ComponentRegistration extends Vue {
   @Prop() public course: string;
   public dataShapes: DataShapeRegistrationStatus[] = [];
+  public questions: QuestionRegistrationStatus[] = [];
 
   public created() {
     const dataShapeData = Courses.allDataShapes().filter((shape) => {
@@ -64,6 +81,29 @@ export default class ComponentRegistration extends Vue {
       });
     });
 
+    const questionData = Courses.getCourse(this.course)!.questions;
+
+    questionData.forEach((question) => {
+      getDoc<QuestionData>(NameSpacer.getQuestionString({
+        course: this.course,
+        questionType: question.name
+      })).then((doc) => {
+        this.questions.push({
+          name: question.name,
+          registered: true,
+          question
+        });
+      }).catch((err) => {
+        this.questions.push({
+          name: question.name,
+          registered: false,
+          question
+        });
+      }).then(() => {
+        this.questions = _.sortBy(this.questions, ['registered', 'name']);
+      });
+    });
+
   }
 
   private registerShape(shapeName: string) {
@@ -72,6 +112,13 @@ export default class ComponentRegistration extends Vue {
     })!;
 
     putDataShape(this.course, shape.dataShape);
+  }
+  private registerQuestion(questionName: string) {
+    const question = this.questions.find((q) => {
+      return q.name === questionName;
+    })!;
+
+    putQuestionType(this.course, question.question);
   }
 
 }
