ff863dbad8146df2e454b2f0154c9c3333afe847
nilock <colinkennedy@hotmail.com>
2018-07-17 11:52:45 -0600

    #16 Add Study link to page

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/edit">Edit</router-link>
+      <router-link to="/edit">Edit</router-link> |
+      <router-link to="/study">Study</router-link>
     </div>
     <router-view/>
   </div>
