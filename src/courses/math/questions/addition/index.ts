5b775cc4d7083f31aed8c5da2a7d7a43409681d7
nilock <colinkennedy@hotmail.com>
2018-07-18 11:53:10 -0600

    Remove old debug code

--- src/courses/math/questions/addition/index.ts
+++ src/courses/math/questions/addition/index.ts
@@ -38,8 +38,6 @@ export class SingleDigitAdditionQuestion extends Question {
     }
 
     public isCorrect(answer: Answer) {
-        alert(`a: ${this.a}, b: ${this.b}, answer: ${answer}`);
-        alert(`this.a + this.b: ${this.a + this.b}`);
         return (1 * this.a) + this.b === answer;
     }
 
