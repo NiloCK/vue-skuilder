44faa8ae8eaf7481ab29ae59fec6cdede31ebd60
nilock <colinkennedy@hotmail.com>
2018-07-18 11:34:25 -0600

    #16 Load random card from db and display it

--- src/views/Study.vue
+++ src/views/Study.vue
@@ -11,11 +11,17 @@
 <script lang="ts">
 import { VueConstructor } from 'vue';
 import Vue from 'vue';
-import { DisplayableData, DocType } from '@/db/types';
+import { DisplayableData, DocType, CardData } from '@/db/types';
 import Viewable from '@/base-course/Viewable';
 import { Component } from 'vue-property-decorator';
 import CardViewer from '@/components/Study/CardViewer.vue';
 import Courses from '@/courses';
+import { getCards, getDoc } from '@/db';
+import { ViewData, displayableDataToViewData } from '@/base-course/Interfaces/ViewData';
+
+function randInt(n: number) {
+    return Math.floor(Math.random() * n);
+}
 
 @Component({
     components: {
@@ -23,31 +29,33 @@ import Courses from '@/courses';
     }
 })
 export default class Study extends Vue {
-    public view: VueConstructor<Vue> | undefined;
-    // public data: DisplayableData = {
-    //     id_datashape: 'asdf',
-    //     data: [
-    //         {
-    //             data: 7,
-    //             name: 'a'
-    //         },
-    //         {
-    //             data: 4,
-    //             name: 'b'
-    //         }
-    //     ],
-    //     course: 'math',
-    //     docType: DocType.DISPLAYABLE_DATA
-    // }
-    public data: any = {
-        a: 4,
-        b: 6
-    };
+    public view: VueConstructor<Vue>;
+
+    public data: ViewData[] = [];
 
     public created() {
-        // view lookup has changed since #22
-        // this.view = getView(Courses, 'math.SingleDigitDivisionQuestion.DivisionHorizontal');
-
+        getCards().then((results) => {
+            return results.docs[
+                randInt(results.docs.length)
+            ];
+        }).then((doc) => {
+            return getDoc<CardData>(doc._id);
+        }).then((cardData) => {
+            this.view = Courses.getView(cardData.id_view);
+            return cardData.id_displayable_data;
+        }).then((displayables) => {
+            return displayables.map((id) => {
+                return getDoc<DisplayableData>(id);
+            });
+        }).then((displayDocs) => {
+            displayDocs.forEach((promiseDoc) => {
+                promiseDoc.then((doc) => {
+                    this.data.push(
+                        displayableDataToViewData(doc)
+                    );
+                });
+            });
+        });
     }
 }
 </script>
