f06b707a8b1fc3dce27a6334d3ca7ae94f7f8af5
nilock <colinkennedy@hotmail.com>
2018-07-18 11:24:29 -0600

    #16 Update data type

--- src/components/Study/CardViewer.vue
+++ src/components/Study/CardViewer.vue
@@ -10,13 +10,14 @@ import { Component, Prop } from 'vue-property-decorator';
 import { DisplayableData } from '@/db/types';
 import Courses from '@/courses';
 import Viewable from '@/base-course/Viewable';
+import { ViewData } from '@/base-course/Interfaces/ViewData';
 
 @Component({
     components: Courses.allViews()
 })
 export default class CardViewer extends Vue {
     @Prop() public view: VueConstructor<Viewable>;
-    @Prop() public data: DisplayableData;
+    @Prop() public data: ViewData[];
 }
 </script>
 
