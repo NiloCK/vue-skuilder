5cd7b5b3b9d14e5a70361b23e7f08e47dafeb873
nilock <colinkennedy@hotmail.com>
2018-07-20 12:17:29 -0600

    #25 Add String, Number inputs

---
+++ src/base-course/Components/UserInput/UserInputString.vue
@@ -0,0 +1,27 @@
+<template>
+    <input
+        class="userInput"
+        v-model="answer"
+        @keyup.enter="submitAnswer(answer)"
+    />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import UserInput from './UserInput';
+
+@Component({})
+export default class UserInputString extends UserInput {
+    public mounted() {
+        this.$el.focus();
+    }
+}
+</script>
+
+<style scoped>
+.userInput {
+  border: none;
+  text-align: center;
+  border-bottom: 1px black;
+}
+</style>
