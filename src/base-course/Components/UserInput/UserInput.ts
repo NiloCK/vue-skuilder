32ef1008ab9dd9f51faacf518ecb2983201e99c5
nilock <colinkennedy@hotmail.com>
2018-07-23 11:38:39 -0600

    #25 Improve the locating of QuestionView parent

--- src/base-course/Components/UserInput/UserInput.ts
+++ src/base-course/Components/UserInput/UserInput.ts
@@ -22,15 +22,42 @@ export default abstract class UserInput extends Vue {
     }
 
     private submit(answer: Answer) {
-        if (this.isQuestionView(this.$parent)) {
-            this.$parent.submitAnswer(
-                answer
-            );
-        } else {
-            const err: string = `UserInput.submit() has failed.`;
-            log(err);
-            throw new Error(err);
+        return this.getQuestionViewAncestor().submitAnswer(answer);
+
+        // if (this.isQuestionView(this.$parent)) {
+        //     this.$parent.submitAnswer(
+        //         answer
+        //     );
+        // } else {
+        //     const err: string = `
+        //     UserInput.submit() has failed.
+        //     The parent element (${this.$parent}) is not a QuestionView element.
+        //     `;
+        //     log(err);
+        //     throw new Error(err);
+        // }
+    }
+
+    private getQuestionViewAncestor(): QuestionView<Question> {
+        let ancestor = this.$parent;
+
+        let count = 0;
+
+        while (!this.isQuestionView(ancestor)) {
+            ancestor = ancestor.$parent;
+            count++;
+
+            if (count > 100) {
+                const err: string =
+                    `
+UserInput.submit() has failed.
+The input element has no QuestionView ancestor element.`;
+                log(err);
+                throw new Error(err);
+            }
         }
+
+        return ancestor;
     }
 
 }
