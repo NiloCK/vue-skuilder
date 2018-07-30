58ee68e10512a679a6235744a3ea826c133063e5
nilock <colinkennedy@hotmail.com>
2018-07-30 10:39:56 -0600

    #29 Attachment data now forwarded to the viewData

--- src/base-course/Interfaces/ViewData.ts
+++ src/base-course/Interfaces/ViewData.ts
@@ -9,5 +9,14 @@ export function displayableDataToViewData(data: DisplayableData): ViewData {
     data.data.forEach((field) => {
         ret[field.name] = field.data;
     });
+    if (data._attachments) {
+        Object.getOwnPropertyNames(data._attachments).
+            forEach((attachment) => {
+                // this 2nd check shouldn't be necessary, but TS is insisting
+                if (data._attachments) {
+                    ret[attachment] = data._attachments[attachment].data as Blob;
+                }
+            });
+    }
     return ret;
 }
