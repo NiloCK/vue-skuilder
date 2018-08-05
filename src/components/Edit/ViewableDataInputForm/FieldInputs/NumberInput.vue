942a746ec5cee2e26cfdae8522d57793a282d11e
nilock <colinkennedy@hotmail.com>
2018-08-04 21:43:37 -0300

    Vuetify NumberInput

--- src/components/Edit/ViewableDataInputForm/FieldInputs/NumberInput.vue
+++ src/components/Edit/ViewableDataInputForm/FieldInputs/NumberInput.vue
@@ -1,37 +1,27 @@
 <template>
-    <div>
-        <label v-bind:for="field.name">{{field.name}}: </label>
-        <input
-            v-model="store[field.name]"
-            v-bind:name="field.name"
-            @change="validate"
-            type="number"            
-        />
-    </div>
+    <v-text-field
+        box
+        type="number"
+        v-model="store[field.name]"
+        :name="field.name"
+        :label="field.name"
+        @change="validate"
+        :rules="vuetifyRules()"
+    />
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import { Component, Prop } from 'vue-property-decorator';
-import { FieldDefinition } from '@/base-course/Interfaces/FieldDefinition';
-import { ValidatingFunction } from '@/base-course/Interfaces/ValidatingFunction';
+import { Component } from 'vue-property-decorator';
 import { numberValidator } from './typeValidators';
 import { FieldInput } from '@/components/Edit/ViewableDataInputForm/FieldInput';
 
 @Component({})
 export default class NumberInput extends FieldInput {
 
-  public getValidators(): ValidatingFunction[] {
-    const ret = [numberValidator];
-
-    if (this.field.validator) {
-        ret.push(this.field.validator.test);
+    public get validators() {
+        const ret = super.validators;
+        ret.unshift(numberValidator);
+        return ret;
     }
-    return ret;
-  }
 }
 </script>
-
-<style scoped>
-@import './FieldInput.css'
-</style>
