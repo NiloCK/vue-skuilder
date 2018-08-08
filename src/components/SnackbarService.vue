d972fde41cb0611c91928a5ee786ee9eb3513076
nilock <colinkennedy@hotmail.com>
2018-08-08 11:50:36 -0300

    #33 Remove div wrapper

--- src/components/SnackbarService.vue
+++ src/components/SnackbarService.vue
@@ -1,21 +1,19 @@
 <template>
-  <div>
-    <v-snackbar
+  <v-snackbar
       v-model="show"
       :timeout="currentSnack.timeout"
       bottom
       right
+  >
+    {{ currentSnack.text }}
+    <v-btn
+      color="pink"
+      flat
+      @click="show = false"
     >
-      {{ currentSnack.text }}
-      <v-btn
-          color="pink"
-          flat
-          @click="show = false"
-        >
-          Close
-        </v-btn>
-    </v-snackbar>
-  </div>
+      Close
+    </v-btn>
+  </v-snackbar>
 </template>
 
 <script lang="ts">
