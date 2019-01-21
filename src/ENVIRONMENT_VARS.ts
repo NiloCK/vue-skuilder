33401341a67fa631707ac00995be1bcab345d899
Colin Kennedy <colin@mrkennedy.ca>
2019-01-21 14:54:31 -0700

    #38 Add a .env.development file

--- src/ENVIRONMENT_VARS.ts
+++ src/ENVIRONMENT_VARS.ts
@@ -1,3 +1,21 @@
+interface Environment {
+    COUCHDB_SERVER_URL: string
+    DEBUG: boolean
+}
+
+let env: Environment = {
+    COUCHDB_SERVER_URL: '',
+    DEBUG: false
+};
+
+env.COUCHDB_SERVER_URL = process.env.VUE_APP_COUCHDB_SERVER!;
+
+if (process.env.VUE_APP_DEBUG !== undefined) {
+    env.DEBUG = process.env.VUE_APP_DEBUG === 'true';
+}
+
+export default env;
+
 /**
  * URL to the remote couchDB instance that the app connects to.
  *
@@ -6,7 +24,7 @@
  */
 export const remote_couch_url: string =
     // 'devRemote';
-    'http://localhost:5984/';
+    process.env.VUE_APP_COUCHDB_SERVER!;
 /**
  * A global flag to enable debug messaging mode for different libraries
  * in the project.
