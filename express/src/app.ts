36425b21ede65da2aa6f488f4d00e280a1be7073
Colin Kennedy <colin@mrkennedy.ca>
2019-01-28 15:48:32 -0700

    #45

--- express/src/app.ts
+++ express/src/app.ts
@@ -1,15 +1,16 @@
-import express = require('express');
-import http = require('http');
-const app = express()
-const port = 3000
-import pouch = require('pouchdb');
 import * as Nano from 'nano';
+import * as express from 'express';
+import * as http from 'http';
+import * as pouch from 'pouchdb';
 const dotenv = require('dotenv');
+
+
+const port = 3000
+const app = express()
 
 dotenv.config({
     path: './.env.development'
 });
-
 
 const couchURL = process.env.VUE_APP_COUCHDB;
 const debug = process.env.VUE_APP_DEBUG;
@@ -22,8 +23,8 @@ const admin = {
     username: process.env.VUE_APP_COUCH_ADMIN,
     password: process.env.VUE_APP_COUCH_PASSWORD
 }
-const credentialCouchURL = 
- `http://${admin.username}:${admin.password}@${couchURL}`;
+const credentialCouchURL =
+    `http://${admin.username}:${admin.password}@${couchURL}`;
 
 console.log(
     `url: ${couchURL}
@@ -37,37 +38,32 @@ let Couch = Nano(credentialCouchURL);
 
 let count = 5;
 
+function createClassroom(name: string, teacher: string) {
+    const studentDbName: string = `classdb-student-${name}`;
+    const teacherDbName: string = `classdb-teacher-${name}`;
+
+    Couch.db.create(studentDbName).then((resp) => {
+        if (resp.ok) {
+            return Couch.db.create(teacherDbName);
+        }
+    }).then((resp) => {
+        if (resp.ok) {
+            let stuDb = Couch.use(studentDbName);
+            stuDb.get('_security').then((secDoc) => {
+                (secDoc as any).admins = [teacher];
+                (secDoc as any).members = [];
+                stuDb.insert(secDoc);
+            });
+        }
+    });
+
+
+}
+
 app.get('/', (req, res) => {
 
-    console.log("Start");
-    // var x = http.request( options, (httpRes) => {
-    //     console.log("Connected");
-    //     httpRes.on('data',function(data){
-    //         console.log(data);
-    //     });
-    // });
-    try {
-        count++;
-
-        var db = new pouch('http://nilock:password@localhost:5984/testdb' + count,
-            {
-                // skip_setup: true,
-                auth: {
-                    username: 'nilock',
-                    password: 'password'
-                }
-            });
-
-        db.info().then((doc: any) => {
-            console.log(`db info is: ${doc}`);
-            console.log(`count is: ${count}`);
-        }).catch((e: any) => {
-            console.log(`NO INFO: ${e.reason}`);
-        })
-    } catch (e) {
-        console.log(`Error is: ${e}`);
-    }
-    res.send('Hello World!');
+    console.log("Creating a classroom.........");
+    createClassroom('testclass' + Math.random().toPrecision(10), 'Colin');
 
 });
 
