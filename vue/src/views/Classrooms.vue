ffb4f561e54c351fa0db361ce87267afe209ffb4
nilock <colinkennedy@hotmail.com>
2019-02-12 06:43:15 -0700

    #45 Add classrooms view

---
+++ vue/src/views/Classrooms.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Hi
+    <v-btn>Create a classroom</v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import SkldrVue from '@/SkldrVue';
+
+@Component({
+  components: {
+  }
+})
+export default class Classroom extends Vue {
+  public test: string = 'hello';
+}
+</script>
