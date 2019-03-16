d2685636597aff0863d0a40860a5af250e4d8b5e
nilock <colinkennedy@hotmail.com>
2019-03-16 02:33:40 -0600

    Config fix so Question type names are preserved
    (... in production build)

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,8 +1,25 @@
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     configureWebpack: {
         devtool: 'source-map',
-
+        optimization: {
+            minimize: false // works on its own, but makes a bigger output file
+            // namedChunks: true,                 // | 
+            // chunkIds: 'named',                 // | 
+            // namedModules: true,                // | 
+            // moduleIds: "named",                // | 
+            // usedExports: false                 //  \
+            // minimizer: [                       //   \
+            //     new TerserPlugin({             //    =-  These do not work.
+            //         terserOptions: {           //    =-  Class names are not emitted
+            //             mangle: false,         //   /
+            //             keep_classnames: true, //  /
+            //             keep_fnames: true      // |
+            //         }                          // |
+            //     })                             // |
+            // ]                                  // |    
+        }
     },
     baseUrl: process.env.NODE_ENV === 'production' ?
         '/skuilder/'
