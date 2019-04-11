bd88651fdc875fefed5f15e7d1e0b8c82635d6f4
nilock <colinkennedy@hotmail.com>
2019-04-11 07:30:24 -0600

    Image dragging WIP

--- vue/src/components/Edit/ViewableDataInputForm/FieldInputs/ImageInput.vue
+++ vue/src/components/Edit/ViewableDataInputForm/FieldInputs/ImageInput.vue
@@ -1,16 +1,20 @@
 <template>
     <div>
         <label v-bind:for="field.name">{{field.name}}: </label>
-        <input
-            ref="inputField"
-            v-bind:id="blobInputID"
-            v-bind:name="field.name"
-            @change="processInput"
-            type="file"
-            v-bind:class="validationStatus.status"
+        <div 
+          v-on:drop="dropHandler"
+          v-on:dragover="dragHandler"
+          v-on:click="dragHandler"
         >
-        <div @ondrop="dropHandler(event)" @ondragover="dragHandler(event)">
             Drop a file here...
+          <input
+              ref="inputField"
+              v-bind:id="blobInputID"
+              v-bind:name="field.name"
+              @change="processInput"
+              type="file"
+              v-bind:class="validationStatus.status"
+          >
         </div>
     </div>
 </template>
@@ -24,27 +28,35 @@ import { FieldInput } from '../FieldInput';
 import { log } from 'util';
 
 @Component
-export default class BlobInput extends FieldInput {
-  public dropHandler(ev: Event) {
+export default class ImageInput extends FieldInput {
+  public dropHandler(ev: DragEvent) {
     if (ev) {
-
       ev.preventDefault();
-      log(`Dropped`);
+
+      // const item = ev.dataTransfer.items[0];
+      // const img = item.getAsFile()!;
+
+      // this.setData({
+      //   content_type: img.type,
+      //   data: img.slice()
+      // });
+      // this.validate();
+
+      const imgURL = ev.dataTransfer!.getData('text');
+      this.fetchImg(imgURL);
+
+      log(`Dropped: ${imgURL}`);
       //   this.removeDragData(ev as DragEvent);
+    } else {
+      log('no event');
     }
   }
-  public dragHandler(ev: Event) {
+  public dragHandler(ev: DragEvent) {
     if (ev) {
       ev.preventDefault();
-      log('Dragging...');
-    }
-  }
-
-  private removeDragData(ev: DragEvent) {
-    if (ev.dataTransfer.items) {
-      ev.dataTransfer.items.clear();
+      log(`Dragging... ${JSON.stringify(ev)}`);
     } else {
-      ev.dataTransfer.clearData();
+      log('no event');
     }
   }
 
@@ -56,6 +68,14 @@ export default class BlobInput extends FieldInput {
     }
   }
 
+  private removeDragData(ev: DragEvent) {
+    if (ev.dataTransfer!.items) {
+      ev.dataTransfer!.items.clear();
+    } else {
+      ev.dataTransfer!.clearData();
+    }
+  }
+
   private get blobInputID(): string {
     return 'blobInput' + this.field.name;
   }
@@ -64,6 +84,20 @@ export default class BlobInput extends FieldInput {
     return document.getElementById(
       this.blobInputID
     ) as HTMLInputElement;
+  }
+
+  private async fetchImg(url: string) {
+    const img = await fetch(url, {
+      'mode': 'no-cors',
+      'content-type': 'image'
+    } as any);
+    const blob = await (img.body as any).blob();
+
+    const file = new File(blob, '');
+    this.setData({
+      content_type: file.type,
+      data: file.slice()
+    });
   }
 
   private async processInput() {
