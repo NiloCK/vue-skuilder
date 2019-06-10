9ea9774e817d3dbdd6b9eed6544d974be34405de
nilock <colinkennedy@hotmail.com>
2019-06-09 21:59:16 -0600

    Make SkldrVue a concrete class

--- vue/src/SkldrVue.ts
+++ vue/src/SkldrVue.ts
@@ -2,6 +2,6 @@ import Vue from 'vue';
 import { Store } from 'vuex';
 import { AppState } from '@/store';
 
-export default abstract class SkldrVue extends Vue {
+export default class SkldrVue extends Vue {
   public $store: Store<AppState>;
 }
