d9afb95e2dbc3bab1233c0ae1804cd048f4d28c3
nilock <colinkennedy@hotmail.com>
2019-06-22 10:24:08 -0600

    Add job status tracking and per-job result return

--- express/src/utils/processQueue.ts
+++ express/src/utils/processQueue.ts
@@ -1,6 +1,7 @@
 
 interface Request { }
 interface Result {
+    status: 'ok' | 'awaiting' | 'warning' | 'error';
     ok: boolean;
     error?: any;
 }
@@ -32,16 +33,97 @@ export default class AsyncProcessQueue<T extends Request> {
     private processing: boolean = false;
     private nextID: number = 0;
 
-    public addRequest(req: T) {
+    /**
+     * Returns 'complete' if the job is complete, 'error' if the
+     * job failed, and the job's position in queue if not yet
+     * completed.
+     * 
+     * @param jobID The jobID returned by addRequest
+     */
+    public jobStatus(jobID: number): 'complete' | 'error' | number {
+        let ret: any;
+        this.queue.forEach((req) => {
+            if (req.id === jobID) {
+                ret = this.queue.indexOf(req);
+            }
+        });
+
+        this.completed.forEach((req) => {
+            if (req.id === jobID) {
+                ret = 'complete';
+            }
+        });
+
+        this.errors.forEach((req) => {
+            if (req.id === jobID) {
+                ret = 'error';
+            }
+        });
+
+        return ret;
+    }
+
+    private async recurseGetResult(jobID: number, depth: number): Promise<Result> {
+        // polling intervals in milliseconds
+        console.log(`Checking job status of job ${jobID}...`);
+        const intervals = [100, 200, 400, 800, 1000, 2000, 3000, 5000];
+        depth = Math.min(depth, intervals.length - 1);
+
+        let status: 'complete' | 'error' | number;
+
+        const p = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                status = this.jobStatus(jobID);
+                if (status === 'complete' || status === 'error') {
+                    resolve();
+                } else {
+                    reject();
+                }
+            }, intervals[depth], jobID)
+        })
+
+        return p.then(() => {
+            return this.getResult(jobID);
+        }).catch(() => {
+            return this.recurseGetResult(jobID, depth + 1);
+        });
+    }
+
+    public async getResult(jobID: number, depth: number = 0): Promise<Result> {
+        const status = this.jobStatus(jobID);
+
+        if (status === 'complete') {
+            const res = this.completed.find((val) => {
+                return val.id === jobID;
+            });
+            return res.result;
+        } else if (status === 'error') {
+            const res = this.errors.find((val) => {
+                return val.id === jobID;
+            });
+            return {
+                error: res.error,
+                ok: false,
+                status: 'error'
+            };
+        } else {
+            return this.recurseGetResult(jobID, 0);
+        }
+    }
+
+    public addRequest(req: T): number {
+        const id: number = this.nextID++;
 
         this.queue.push({
-            id: this.nextID++,
+            id: id,
             request: req
         });
 
         if (!this.processing) {
             this.process();
         }
+
+        return id;
     }
 
     /**
