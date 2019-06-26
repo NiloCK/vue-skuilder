6b6cd65b774b3c80079faadb57b267ab53bb63ca
Colin Kennedy <colin@mrkennedy.ca>
2019-06-26 12:03:37 -0600

    55 - use CouchDB uuid for doc insert

--- express/src/client-requests/course-requests.ts
+++ express/src/client-requests/course-requests.ts
@@ -3,6 +3,7 @@ import { docCount, useOrCreateDB } from '../app';
 import CouchDB from '../couchdb';
 import AsyncProcessQueue, { Result } from '../utils/processQueue';
 import { CreateCourse, DeleteCourse } from '../../../vue/src/server/types';
+import nano = require('nano');
 
 const COURSE_DB_LOOKUP = 'coursedb-lookup';
 const courseHasher = new hashids(
@@ -13,15 +14,11 @@ const courseHasher = new hashids(
 
 async function createCourse(cfg: CreateCourse['data']): Promise<any> {
     const lookup = await useOrCreateDB(COURSE_DB_LOOKUP);
-    const num = await docCount(COURSE_DB_LOOKUP) + 1;
-    const uuid = (await CouchDB.uuids(1)).uuids[0];
-
-    const courseID = courseHasher.encode(num);
 
     const lookupInsert = await lookup.insert({
-        _id: courseID,
         ...cfg
-    });
+    } as nano.MaybeDocument);
+    const courseID = lookupInsert.id;
 
     const courseDBName: string = `coursedb-${courseID}`;
     const dbCreation = await CouchDB.db.create(courseDBName);
