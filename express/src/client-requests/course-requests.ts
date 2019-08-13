42c4e53919e1caea4b577c073e79d56b1a1a4b83
nilock <colinkennedy@hotmail.com>
2019-08-13 10:52:35 -0600

    #55 Private course now has auth restrictions on db
    (On creation. To do: invite / add other users, mods, admin, etc).

--- express/src/client-requests/course-requests.ts
+++ express/src/client-requests/course-requests.ts
@@ -1,10 +1,11 @@
 import hashids from 'hashids';
-import { docCount, useOrCreateDB } from '../app';
+import { docCount, useOrCreateDB, SecurityObject } from '../app';
 import CouchDB from '../couchdb';
 import AsyncProcessQueue, { Result } from '../utils/processQueue';
 import { CreateCourse, DeleteCourse } from '../../../vue/src/server/types';
 import nano = require('nano');
 import { postProcessCourse } from '../attachment-preprocessing';
+
 
 export const COURSE_DB_LOOKUP = 'coursedb-lookup';
 const courseHasher = new hashids(
@@ -13,23 +14,42 @@ const courseHasher = new hashids(
     'abcdefghijkmnopqrstuvwxyz23456789'
 );
 
-async function createCourse(cfg: CreateCourse['data']): Promise<any> {
+type CourseConfig = CreateCourse['data'];
+
+async function createCourse(cfg: CourseConfig): Promise<any> {
     const lookup = await useOrCreateDB(COURSE_DB_LOOKUP);
 
     const lookupInsert = await lookup.insert({
         ...cfg
     } as nano.MaybeDocument);
     const courseID = lookupInsert.id;
+    cfg.courseID = courseID;
 
     const courseDBName: string = `coursedb-${courseID}`;
     const dbCreation = await CouchDB.db.create(courseDBName);
 
-    if (dbCreation) {
+    if (dbCreation.ok) {
         const courseDB = CouchDB.use(courseDBName);
+
         courseDB.insert({
             _id: 'CourseConfig',
             ...cfg
         });
+
+        if (!cfg.public) {
+            const secObj: SecurityObject = {
+                admins: {
+                    names: [],
+                    roles: []
+                },
+                members: {
+                    names: [cfg.creator],
+                    roles: []
+                }
+            }
+
+            courseDB.insert(secObj as nano.MaybeDocument, '_security');
+        }
     };
 
     // follow the course so that user-uploaded content goes through
