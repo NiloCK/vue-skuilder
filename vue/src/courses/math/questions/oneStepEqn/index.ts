1bbcca6a74e072b04e80b3099c53a83f213aef24
nilock <colinkennedy@hotmail.com>
2019-10-01 23:57:24 -0600

    Add one-step equation questions

---
+++ vue/src/courses/math/questions/oneStepEqn/index.ts
@@ -0,0 +1,134 @@
+import { Answer, Question } from '@/base-course/Displayable';
+import { FieldDefinition } from '@/base-course/Interfaces/FieldDefinition';
+import { ViewData } from '@/base-course/Interfaces/ViewData';
+import { DataShapeName } from '@/enums/DataShapeNames';
+import { FieldType } from '@/enums/FieldType';
+import { Status } from '@/enums/Status';
+import { Validator } from '@/base-course/Interfaces/Validator';
+import Solve from './solve.vue';
+import { log } from 'util';
+
+const validator = {
+  instructions: 'An integer between 0 and 10, inclusive.',
+  test: (value: string) => {
+    const input = parseInt(value, 10);
+    if (0 <= input && input <= 100) {
+      return {
+        status: Status.ok,
+        msg: ''
+      };
+    } else {
+      return {
+        status: Status.error,
+        msg: 'Single-step equation problem inputs must be between 0 and 100, inclusive.'
+      };
+    }
+  }
+};
+
+const operationValidator: Validator = {
+  test: (val: string) => {
+    if (
+      val === Operation.ADDITION ||
+      val === Operation.SUBTRACTION ||
+      val === Operation.MULTIPLICATION ||
+      val === Operation.DIVISION
+    ) {
+      return {
+        status: Status.ok,
+        msg: ''
+      };
+    } else {
+      return {
+        status: Status.error,
+        msg: `Operation must be ADDITION, SUBTRACTION, MULTIPLICATION, or DIVISION`
+      };
+    }
+  }
+};
+
+const fields: FieldDefinition[] = [
+  {
+    name: 'a',
+    type: FieldType.INT,
+    validator
+  },
+  {
+    name: 'b',
+    type: FieldType.INT,
+    validator
+  },
+  {
+    name: 'operation',
+    type: FieldType.STRING,
+    validator: operationValidator
+  }
+];
+
+enum Operation {
+  ADDITION = 'ADDITION',
+  SUBTRACTION = 'SUBTRACTION',
+  MULTIPLICATION = 'MULTIPLICATION',
+  DIVISION = 'DIVISION'
+}
+
+export class OneStepEquation extends Question {
+  public static dataShapes = [{
+    name: DataShapeName.MATH_OneStepEquation,
+    fields
+  }];
+
+  public static views = [
+    Solve
+  ];
+
+  public a: number;
+  public b: number;
+  public operation: Operation;
+
+  constructor(data: ViewData[]) {
+    super(data);
+    this.a = data[0].a as number;
+    this.b = data[0].b as number;
+    this.operation = data[0].operation as Operation;
+  }
+
+  public answer(): number {
+    let answer: number = 0;
+
+    if (this.operation === Operation.ADDITION) {
+
+      // x - a = b
+      answer = this.a + this.b;
+    } else if (this.operation === Operation.SUBTRACTION) {
+      // x + a = b
+      answer = this.b - this.a;
+    } else if (this.operation === Operation.MULTIPLICATION) {
+      // x / a = b
+      answer = this.a * this.b;
+    } else if (this.operation === Operation.DIVISION) {
+      // ax = ab
+      answer = this.b;
+    }
+
+    log(
+      `The answer is ${answer}
+The operation is ${this.operation.valueOf()}
+this.a = ${this.a}
+this.b = ${this.b}
+`);
+    return answer;
+  }
+
+  public isCorrect(userAnswer: Answer) {
+    return userAnswer === this.answer();
+  }
+
+  public dataShapes() {
+    return OneStepEquation.dataShapes;
+  }
+
+  public views() {
+    return OneStepEquation.views;
+  }
+}
---
+++ vue/src/courses/math/questions/oneStepEqn/solve.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h4>Solve the Equation</h4>
+    <br><br>
+      <div v-if="question.operation.valueOf() === 'ADDITION'">
+        {{ variable || 'r' }} - {{ question.a }} = {{ question.b }}
+      </div>
+      <div v-else-if="question.operation.valueOf() === 'SUBTRACTION'">
+        {{ variable || 's' }} + {{ question.a }} = {{ question.b }}
+      </div>
+      <div v-else-if="question.operation.valueOf() === 'MULTIPLICATION'">
+        {{ variable || 'q' }} &#247; {{ question.a }} = {{ question.b }}
+      </div>
+      <div v-else-if="question.operation.valueOf() === 'DIVISION'">
+        {{ variable || 'm' }} * {{ question.a }} = {{ question.b * question.a }}
+      </div>
+      <div v-else>
+        No operation!? (This should never show)
+      </div>
+      
+      <br><br>
+      
+      {{ variable }} = <UserInputNumber />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { QuestionView } from '@/base-course/Viewable';
+import { OneStepEquation } from './index';
+import UserInputNumber from '@/base-course/Components/UserInput/UserInputNumber.vue';
+import { randomInt } from '../../utility';
+import { log } from 'util';
+
+@Component({
+  components: {
+    UserInputNumber
+  }
+})
+export default class Solve extends QuestionView<OneStepEquation> {
+  public answer: string = '';
+  public variable: string = 'r';
+
+  constructor() {
+    super();
+    const vars = ['a', 'b', 'd', 'A', 'z', 'B', 'd', 'y'];
+    this.variable = vars[
+      randomInt(0, vars.length)
+    ];
+  }
+  get question() {
+    return new OneStepEquation(this.data);
+  }
+
+  public submit() {
+    alert(this.question.isCorrect(parseInt(this.answer, 10)));
+  }
+}
+</script>
