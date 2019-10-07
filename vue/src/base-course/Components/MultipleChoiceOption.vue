bfe99df509e6b8f4813193b0798ab2b2916bc3f3
nilock <colinkennedy@hotmail.com>
2019-10-07 00:21:30 -0600

    Update Radio Choice w/ disabled 'wrong' answers,

--- vue/src/base-course/Components/MultipleChoiceOption.vue
+++ vue/src/base-course/Components/MultipleChoiceOption.vue
@@ -15,69 +15,83 @@ import { Component, Prop } from 'vue-property-decorator';
 
 @Component({})
 export default class MultipleChoiceOption extends Vue {
-    @Prop() public content: string;
-    @Prop() public selected: boolean;
-    @Prop() public number: number;
-    @Prop() public setSelection: (selection: number) => void;
-    @Prop() public submit: () => void;
+  @Prop() public content: string;
+  @Prop() public selected: boolean;
+  @Prop() public number: number;
+  @Prop() public setSelection: (selection: number) => void;
+  @Prop() public submit: () => void;
+  @Prop() public markedWrong: boolean;
 
-    public select(): void {
-        this.setSelection(this.number);
-    }
+  public select(): void {
+    this.setSelection(this.number);
+  }
 
-    public submitThisOption(): void {
-        this.select();
-        this.submit();
+  public submitThisOption(): void {
+    if (this.markedWrong) {
+      return;
+    } else {
+      this.select();
+      this.submit();
     }
+  }
 
-    get className(): string {
-        if (this.selected) {
-            return 'choice selected';
-        } else {
-            return 'choice';
-        }
+  get className(): string {
+    if (this.selected && !this.markedWrong) {
+      return 'choice selected';
+    } else if (!this.selected && !this.markedWrong) {
+      return 'choice';
+    } else if (this.selected && this.markedWrong) {
+      return 'choice selected wrong';
+    } else if (!this.selected && this.markedWrong) {
+      return 'choice wrong';
+    } else {
+      throw new Error(
+        `'selected' and 'markedWrong' props in MultipleChoiceOption are in an impossible configuration.`
+      );
     }
+  }
 }
 </script>
 
 <style scoped>
 .choice {
-    display: inline-block;
-    border-radius: 4px;
-    padding: 15px;
-    margin: 10px;
+  display: inline-block;
+  border-radius: 4px;
+  padding: 15px;
+  margin: 10px;
 }
 
-.selected{
-    transition: all ease-out .15s;
-    box-shadow: 2px 3px 4px 2px;
-    transform:
-        translateY(-10px)
-        /* rotate(3deg) */
-        scale(1.15);
+.selected {
+  transition: all ease-out 0.15s;
+  box-shadow: 2px 3px 4px 2px;
+  transform: translateY(-10px) /* rotate(3deg) */ scale(1.15);
 }
 
-.choice:nth-child(1){
-    background-color: lightgreen;
-    outline-width: 5px; 
+.wrong {
+  background-color: gray !important;
 }
-.choice:nth-child(2){
-    /* section for 'selected' element */
-    /* transform: rotate(45); */
 
-    /* end section */
-    background-color: lightcoral;
+.choice:nth-child(1) {
+  background-color: lightgreen;
+  outline-width: 5px;
 }
-.choice:nth-child(3){
-    background-color: lightskyblue;
+.choice:nth-child(2) {
+  /* section for 'selected' element */
+  /* transform: rotate(45); */
+
+  /* end section */
+  background-color: lightcoral;
 }
-.choice:nth-child(4){
-    background-color: peachpuff;
+.choice:nth-child(3) {
+  background-color: lightskyblue;
 }
-.choice:nth-child(5){
-    background-color: yellowgreen;
+.choice:nth-child(4) {
+  background-color: peachpuff;
 }
-.choice:nth-child(6){
-    background-color: lightsalmon;
+.choice:nth-child(5) {
+  background-color: yellowgreen;
+}
+.choice:nth-child(6) {
+  background-color: lightsalmon;
 }
 </style>
--- vue/src/base-course/Components/RadioMultipleChoice.vue
+++ vue/src/base-course/Components/RadioMultipleChoice.vue
@@ -8,6 +8,7 @@
         :number='choiceList.indexOf(choice)'
         :setSelection='setSelection'
         :submit='forwardSelection'
+        :markedWrong='choiceIsWrong(choice)'
     />
   </div>
 </template>
@@ -17,76 +18,98 @@ import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import MultipleChoiceOption from './MultipleChoiceOption.vue';
 import UserInput from '@/base-course/Components/UserInput/UserInput';
+import { Answer } from '../Displayable';
 
 @Component({
-    components: {
-        MultipleChoiceOption
-    }
+  components: {
+    MultipleChoiceOption
+  }
 })
 export default class RadioSelect extends UserInput {
-    @Prop() public choiceList: string[];
-    @Prop() public MouseTrap: MousetrapInstance;
-    // @Prop() public submit: (selection: number) => void;
+  @Prop() public choiceList: string[];
+  @Prop() public MouseTrap: MousetrapInstance;
+  // @Prop() public submit: (selection: number) => void;
 
-    public currentSelection: number = -1;
+  public currentSelection: number = -1;
+  // todo: incorrect user-answers are greyed out and
+  // disabled for future submission
+  public incorrectSelections: number[] = [];
 
-    public mounted() {
-        this.$el.focus();
+
+  public mounted() {
+    this.$el.focus();
+  }
+
+
+  public created() {
+    this.MouseTrap.bind('left', this.decrementSelection);
+    this.MouseTrap.bind('right', this.incrementSelection);
+    this.MouseTrap.bind('enter', this.forwardSelection);
+
+    for (let i = 0; i < this.choiceList.length; i++) {
+      this.bindNumberKey(i + 1);
     }
+  }
 
-    public created() {
-        this.MouseTrap.bind('left', this.decrementSelection);
-        this.MouseTrap.bind('right', this.incrementSelection);
-        this.MouseTrap.bind('enter', this.forwardSelection);
+  public forwardSelection(): void {
+    if (this.choiceIsWrong(this.choiceList[this.currentSelection])) {
+      return; // do not 'resubmit' greyed-out choices
+    } else if (this.currentSelection !== -1) {
+      const rec = this.submitAnswer({
+        choiceList: this.choiceList,
+        selection: this.currentSelection
+      });
 
-        for (let i = 0; i < this.choiceList.length; i++) {
-            this.bindNumberKey(i + 1);
-        }
+      if (!rec.isCorrect) {
+        this.incorrectSelections.push(this.currentSelection);
+      }
     }
+  }
 
-    public forwardSelection(): void {
-        if (this.currentSelection !== -1) {
-            this.submitAnswer({
-                options: this.choiceList,
-                selection: this.currentSelection
-            });
-        }
+  public setSelection(selection: number): void {
+    if (selection < this.choiceList.length) {
+      this.currentSelection = selection;
     }
+  }
 
-    public setSelection(selection: number): void {
-        if (selection < this.choiceList.length) {
-            this.currentSelection = selection;
-        }
+  public incrementSelection() {
+    // alert('increment');
+    if (this.currentSelection === -1) {
+      this.currentSelection = Math.ceil(this.choiceList.length / 2);
+    } else {
+      this.currentSelection = Math.min(
+        this.choiceList.length - 1,
+        this.currentSelection + 1
+      );
     }
+  }
 
-    public incrementSelection() {
-        // alert('increment');
-        if (this.currentSelection === -1) {
-            this.currentSelection = Math.ceil(this.choiceList.length / 2);
-        } else {
-            this.currentSelection = Math.min(
-                this.choiceList.length - 1,
-                this.currentSelection + 1
-            );
-        }
+  public decrementSelection() {
+    // alert('dencrement');
+    if (this.currentSelection === -1) {
+      this.currentSelection = Math.floor((this.choiceList.length / 2) - 1);
+    } else {
+      this.currentSelection = Math.max(
+        0,
+        this.currentSelection - 1
+      );
     }
+  }
 
-    public decrementSelection() {
-        // alert('dencrement');
-        if (this.currentSelection === -1) {
-            this.currentSelection = Math.floor((this.choiceList.length / 2) - 1);
-        } else {
-            this.currentSelection = Math.max(
-                0,
-                this.currentSelection - 1
-            );
-        }
-    }
+  private choiceIsWrong(choice: string): boolean {
+    let ret: boolean = false;
+    this.incorrectSelections.forEach((sel) => {
+      if (this.choiceList[sel] === choice) {
+        ret = true;
+      }
+    });
+    return ret;
+  }
 
-    private bindNumberKey(n: number): void {
-        this.MouseTrap.bind(n.toString(), () => {
-            this.currentSelection = n - 1;
-        });
-    }
+  private bindNumberKey(n: number): void {
+    this.MouseTrap.bind(n.toString(), () => {
+      this.currentSelection = n - 1;
+    });
+  }
 }
 </script>
