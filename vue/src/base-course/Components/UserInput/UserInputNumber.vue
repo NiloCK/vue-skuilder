2627609c768a31d3442e071063307377a83f28e7
nilock <colinkennedy@hotmail.com>
2019-10-14 15:06:21 -0600

    Update Number input to use vuetify input style

--- vue/src/base-course/Components/UserInput/UserInputNumber.vue
+++ vue/src/base-course/Components/UserInput/UserInputNumber.vue
@@ -1,10 +1,20 @@
 <template>
-    <input
+    <!-- <input
         class='userInput'
         type="number"
         v-model="answer"
         @keyup.enter="submitAnswer(strToNumber(answer))"
-    />
+    /> -->
+    <v-text-field
+        v-model="answer"
+        prepend-icon="edit"
+        @keyup.enter="submitAnswer(strToNumber(answer))"
+        autofocus
+        row-height="24"
+        toggle-keys="[13,32]"
+        type="text"
+        class='headline'
+    ></v-text-field>
 </template>
 
 <script lang="ts">
@@ -13,13 +23,13 @@ import UserInput from './UserInput';
 
 @Component({})
 export default class UserInputNumber extends UserInput {
-    public mounted() {
-        this.$el.focus();
-    }
+  public mounted() {
+    this.$el.focus();
+  }
 
-    private strToNumber(num: string): number {
-        return Number.parseFloat(num);
-    }
+  private strToNumber(num: string): number {
+    return Number.parseFloat(num);
+  }
 }
 </script>
 
