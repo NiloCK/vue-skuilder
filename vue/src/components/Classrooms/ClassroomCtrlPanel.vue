968bdbbe502b3e1970a1fa8176249d3d9d31e376
Colin Kennedy <colin@mrkennedy.ca>
2020-03-26 14:29:02 -0600

    Enable adding / removing assigned content

--- vue/src/components/Classrooms/ClassroomCtrlPanel.vue
+++ vue/src/components/Classrooms/ClassroomCtrlPanel.vue
@@ -1,34 +1,92 @@
 <template>
   <div v-if='!updatePending'>
+      
     
-    <v-container grid-list-md>
-        <h1>{{_classroomCfg.name}}</h1>
+        <h1><router-link to="/classrooms">My Classrooms</router-link> / {{_classroomCfg.name}}</h1>
+
+        <h3>Join code: {{_classroomCfg.joinCode}}</h3>
         <v-layout wrap column>
+          
           <v-flex xs12 sm6 md4>
-            <v-text-field
-              v-model="name"
-              counter="30"
-              :rules="nameRules"
-              label="Class Name"
-              required
-              hint="Eg: Smith, Chemistry, Period 3"
-            ></v-text-field>
+            <v-checkbox
+              label="Allow peer instruction"
+              v-model="_classroomCfg.peerAssist"
+              :value="true"></v-checkbox>
           </v-flex>
-          <v-flex xs12 sm6 md4>
-            <v-checkbox label="Allow peer instruction" v-model="peerAssist" :value="true"></v-checkbox>
-          </v-flex>
-          <v-flex xs12 sm6 md4>
-            <v-select
-              :items="birthYears"
-              label="Approximate Birth Year of Students"
-              v-model="birthYear"
-            ></v-select>
-          </v-flex>
-          <v-flex xs12 sm6 md4>
-            <v-btn :loading="updatePending" color="primary" @click="submit">Create This Class</v-btn>
-          </v-flex>
-        </v-layout>
-      </v-container>
+          <div v-if="classroomDB.ready">
+
+            <h2>Assigned Content:</h2>
+            <h3>Quilts:</h3>
+            <ul></ul>
+            <ul>
+              <li v-for="c in _assignedCourses" :key="c._id">
+                {{c.courseID}} <a @click="removeContent(c)">Remove</a>
+              </li>
+            </ul>
+            <h3>Tags:</h3>
+            <ul>
+              <li v-for="c in _assignedTags" :key="c._id">
+                {{c.courseID}} - {{ c.tagID}}  <a @click="removeContent(c)">Remove</a>
+              </li>
+            </ul>
+            <v-btn 
+              transition="scale-transition"
+              v-if='!addingContent'
+              color="primary"
+              @click='addingContent = true;'    
+            >
+              Assign New Content
+              <v-icon right dark>add</v-icon>
+            </v-btn>
+            <v-card 
+            v-if='addingContent'>
+              <v-toolbar>
+                <v-toolbar-title>Add Content</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn @click="addingContent = false;" right small dark icon color="error" >
+                  <v-icon>close</v-icon>
+                </v-btn>
+              </v-toolbar>
+              <v-card-text>
+                
+              <v-select
+                label="Select Quilt"
+                :items="availableCourses"
+                v-model="selectedCourse"
+                autocomplete
+                item-text='name'
+                item-value='_id'
+                title="Select Quilt"
+              ></v-select>
+              
+              <v-select
+                label="Select Tags"
+                :items="availableTags"
+                item-text='name'
+                item-value='name'
+                v-model="selectedTags"
+                multiple
+                chips
+                hint=""
+                persistent-hint
+              ></v-select>
+              
+              </v-card-text>
+
+              
+              <v-card-actions>
+                <v-btn 
+                  v-if='selectedCourse !== ""'
+                  color="primary"
+                  @click="assignContent"
+                >
+                  {{ selectedTags.length == 0 ? 'Add Entire Quilt' : 'Add Tags'}}
+                  <v-icon flat right dark>add</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </div>
+        </v-layout> 
     
   </div>  
 </template>
@@ -53,8 +111,10 @@ import Mousetrap from "mousetrap";
 import { log } from "util";
 import moment from "moment";
 import { registerUserForClassroom } from '../../db/userDB';
-import { getClassroomDB, CLASSROOM_CONFIG } from '../../db/classroomDB';
-import { Prop } from 'vue-property-decorator';
+import TeacherClassroomDB, { getClassroomDB, CLASSROOM_CONFIG, AssignedContent } from '../../db/classroomDB';
+import { Prop, Watch } from 'vue-property-decorator';
+import { getCourseList, getCourseTagStubs } from '../../db/courseDB';
+import { Tag } from '../../db/types';
 
 @Component({})
 export default class ClassroomCtrlPanel extends SkldrVue {
@@ -63,6 +123,20 @@ export default class ClassroomCtrlPanel extends SkldrVue {
 
   private _classTeacherDB: PouchDB.Database;
   private _classroomCfg: ClassroomConfig;
+
+  private classroomDB: TeacherClassroomDB;
+
+  private _assignedContent: AssignedContent[];
+  private get _assignedCourses() {
+    return this._assignedContent.filter((c) => {
+      return c.type === 'course';
+    });
+  }
+  private get _assignedTags() {
+    return this._assignedContent.filter((c) => {
+      return c.type === 'tag';
+    });
+  }
 
   private nameRules: Array<(value: string) => string | boolean> = [
     value => {
@@ -76,20 +150,61 @@ export default class ClassroomCtrlPanel extends SkldrVue {
   ];
 
   private updatePending: boolean = true;
+  private addingContent: boolean = false;
+  private availableCourses: CourseConfig[] = [];
+  private selectedCourse: string = '';
+  private availableTags: Tag[] = [];
+  private selectedTags: string[] = [];
 
   private async created() {
+    this.classroomDB = new TeacherClassroomDB(this._id);
+    this._assignedContent = await this.classroomDB.getAssignedContent();
+    this._classroomCfg = await this.classroomDB.getConfig();
     log(`Route loaded w/ (prop) _id: ${this._id}`);
     this._classTeacherDB = await getClassroomDB(this._id, 'teacher');
     this._classroomCfg = await this._classTeacherDB.get<ClassroomConfig>(CLASSROOM_CONFIG);
     log(`Config: 
     ${JSON.stringify(this._classroomCfg)}`);
+
+    this.availableCourses = (await getCourseList()).rows.map(r => r.doc!);
+
     this.updatePending = false;
+  }
+
+  @Watch('selectedCourse')
+  private async getCourseTags() {
+    let tags = (await getCourseTagStubs(this.selectedCourse))
+      .rows
+      .map((row) => row.doc!);
+    this.availableTags = tags;
+  }
+
+  private async assignContent() {
+    if (this.selectedTags.length === 0) {
+      await this.classroomDB.assignContent({
+        type: 'course',
+        courseID: this.selectedCourse,
+        assignedBy: this.$store.state.user
+      });
+    } else {
+      await this.selectedTags.forEach((tag) => {
+        this.classroomDB.assignContent({
+          type: 'tag',
+          courseID: this.selectedCourse,
+          tagID: tag,
+          assignedBy: this.$store.state.user,
+        })
+      })
+    }
+
+    this._assignedContent = await this.classroomDB.getAssignedContent();
+  }
+  private async removeContent(c: AssignedContent) {
+    this.classroomDB.removeContent(c);
   }
 
   private async submit() {
     this.updatePending = true;
-
-
   }
 }
 </script>
--- vue/src/db/classroomDB.ts
+++ vue/src/db/classroomDB.ts
@@ -2,9 +2,159 @@ import ENV from '@/ENVIRONMENT_VARS';
 import { ClassroomConfig } from '@/server/types';
 import pouch from 'pouchdb-browser';
 import { pouchDBincludeCredentialsConfig } from '.';
+import moment from 'moment';
 
 const classroomLookupDBTitle = 'classdb-lookup';
 export const CLASSROOM_CONFIG = 'ClassroomConfig';
+
+export type AssignedContent = AssignedCourse | AssignedTag;
+
+interface AssignedTag extends ContentBase {
+  type: 'tag';
+  tagID: string;
+}
+interface AssignedCourse extends ContentBase {
+  type: 'course';
+}
+
+interface ContentBase {
+  type: 'course' | 'tag';
+  /**
+   * Username of the assigning teacher.
+   */
+  assignedBy: string;
+  /**
+   * Date the content was assigned.
+   */
+  assignedOn?: moment.Moment;
+  /**
+   * A 'due' date for this assigned content, for scheduling content
+   * in advance. Content will not be actively pushed to students until
+   * this date.
+   */
+  activeOn?: moment.Moment;
+  courseID: string;
+}
+
+
+/**
+ * Interface for managing a classroom.
+ */
+export default class TeacherClassroomDB {
+  private _db: PouchDB.Database;
+  private _stuDb: PouchDB.Database;
+  private _cfg: ClassroomConfig;
+  private _initComplete: boolean = false;
+
+  constructor(classID: string) {
+    const dbName = `classdb-teacher-${classID}`;
+    const stuDbName = `classdb-student-${classID}`;
+
+    this._db = new pouch(
+      ENV.COUCHDB_SERVER_PROTOCOL + '://' +
+      ENV.COUCHDB_SERVER_URL + dbName,
+      pouchDBincludeCredentialsConfig
+    );
+    this._stuDb = new pouch(
+      ENV.COUCHDB_SERVER_PROTOCOL + '://' +
+      ENV.COUCHDB_SERVER_URL + stuDbName,
+      pouchDBincludeCredentialsConfig
+    );
+    try {
+      this._db.get<ClassroomConfig>(CLASSROOM_CONFIG).then((cfg) => {
+        this._cfg = cfg;
+        this._initComplete = true;
+      });
+    } catch (e) {
+      throw new Error(`Error in TeacherClassroomDB constructor: ${JSON.stringify(e)}`);
+    }
+  }
+
+  public get ready(): boolean {
+    return this._initComplete;
+  }
+
+
+  private readonly _prefix: string = 'content';
+  private getContentId(content: AssignedContent): string {
+    if (content.type === 'tag') {
+      return `${this._prefix}-${content.courseID}-${content.tagID}`;
+    } else {
+      return `${this._prefix}-${content.courseID}`;
+    }
+  }
+
+  public async removeContent(content: AssignedContent) {
+    const contentID = this.getContentId(content);
+
+    this._db.get(contentID).then((doc) => {
+      this._db.remove(doc)
+    }).then(() => {
+      this._db.replicate.to(this._stuDb, {
+        doc_ids: [contentID]
+      });
+    });
+  }
+
+  public async assignContent(content: AssignedContent) {
+
+    let put: PouchDB.Core.Response;
+    let id: string = this.getContentId(content);
+
+    if (content.type === 'tag') {
+      put = await this._db.put<AssignedTag>({
+        courseID: content.courseID,
+        tagID: content.tagID,
+        type: 'tag',
+        _id: id,
+        assignedBy: content.assignedBy,
+        assignedOn: moment.utc(),
+        activeOn: content.activeOn || moment.utc()
+      });
+    } else {
+      put = await this._db.put<AssignedCourse>({
+        courseID: content.courseID,
+        type: 'course',
+        _id: id,
+        assignedBy: content.assignedBy,
+        assignedOn: moment.utc(),
+        activeOn: content.activeOn || moment.utc()
+      });
+    }
+
+    if (put.ok) {
+      this._db.replicate.to(this._stuDb, {
+        doc_ids: [id]
+      });
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public async getAssignedContent(): Promise<AssignedContent[]> {
+    console.log(`Getting assigned content...`);
+    // see couchdb docs 6.2.2:
+    //   Guide to Views -> Views Collation -> String Ranges
+    let docRows = await this._db.allDocs<AssignedContent>({
+      startkey: this._prefix,
+      endkey: this._prefix + `\ufff0`,
+      include_docs: true
+    });
+
+    let ret = docRows.rows.map((row) => {
+      return row.doc!;
+    });
+    console.log(`Assigned content: ${JSON.stringify(ret)}`);
+
+    return ret;
+  }
+
+  public getConfig(): ClassroomConfig {
+
+    return this._cfg;
+  }
+}
 
 const classroomLookupDB: PouchDB.Database = new pouch(
   ENV.COUCHDB_SERVER_PROTOCOL + '://' +
