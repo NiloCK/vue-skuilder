a8c6c9da92253953f333404ba7a3f0bdf501652e
Colin Kennedy <colin@mrkennedy.ca>
2020-03-29 13:14:53 -0600

    Add stub /user view

---
+++ vue/src/views/User.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      User View for {{ _id }}
+    </div>
+</template>
+
+<script lang="ts">
+import Vue, { VueConstructor } from 'vue';
+import { Component, Prop, Emit } from 'vue-property-decorator';
+import { log } from 'util';
+import SkldrVue from '../SkldrVue';
+
+@Component({})
+export default class User extends SkldrVue {
+  @Prop({
+    required: true
+  }) public _id: string;
+}
+</script>
+
+<style scoped>
+</style>
