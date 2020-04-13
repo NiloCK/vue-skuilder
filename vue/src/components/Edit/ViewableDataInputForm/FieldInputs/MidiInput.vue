d04e2f982af3f56cab35f640c72deafaba174458
Colin Kennedy <colin@mrkennedy.ca>
2020-04-13 14:39:14 -0600

    Hack to create multiple new echo qs consecutively
    (I do not understand DataInputForm.vue...)

--- vue/src/components/Edit/ViewableDataInputForm/FieldInputs/MidiInput.vue
+++ vue/src/components/Edit/ViewableDataInputForm/FieldInputs/MidiInput.vue
@@ -1,31 +1,28 @@
 <template>
-    <div>
-      <div v-if='recording'>
-        <span class="headline">Now Recording from device: <span class='font-weight-black'>{{midi.configuredInput}}</span></span>
-      </div>
-      <v-btn 
-        color="primary"
-        @click="play"
-        :disabled='hasRecording()'
-      >
-        Preview <v-icon right>volume_up</v-icon>
-      </v-btn>
-      <v-btn
-       color="error"
-       @click="reset"
-       :disabled='hasRecording()'
-      >
-        Clear and try again <v-icon right>close</v-icon>
-      </v-btn>
+  <div>
+    <div v-if="recording">
+      <span class="headline">
+        Now Recording from device:
+        <span class="font-weight-black">{{midi.configuredInput}}</span>
+      </span>
     </div>
+    <v-btn color="primary" @click="play" :disabled="hasRecording()">
+      Preview
+      <v-icon right>volume_up</v-icon>
+    </v-btn>
+    <v-btn color="error" @click="reset" :disabled="hasRecording()">
+      Clear and try again
+      <v-icon right>close</v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component } from 'vue-property-decorator';
-import { FieldInput } from '../FieldInput';
-import SkMidi from '../../../../courses/piano/utility/midi';
+import { Component } from "vue-property-decorator";
+import { FieldInput } from "../FieldInput";
+import SkMidi from "../../../../courses/piano/utility/midi";
 
-@Component
+@Component({})
 export default class MidiInput extends FieldInput {
   public midi: SkMidi;
   public recording: boolean = false;
@@ -38,8 +35,21 @@ export default class MidiInput extends FieldInput {
 
       this.store[this.field.name] = this.midi.recording;
     } catch (e) {
-      throw e
+      throw e;
     }
+  }
+
+  public clearData() {
+    console.log('midiInput clearing data...');
+    this.midi.stopRecording();
+    this.midi.eraseRecording();
+    this.midi.record();
+    this.recording = true;
+
+    this.store.convertedInput[this.field.name] = this.midi.recording;
+    this.store.validation[this.field.name] = false;
+
+    this.store[this.field.name] = this.midi.recording;
   }
 
   public hasRecording(): boolean {
