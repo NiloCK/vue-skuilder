9081b4509e59cf11792a21c32e769a9a13bc866a
Colin Kennedy <colin@mrkennedy.ca>
2020-04-26 01:34:50 -0600

    Add abstract visualization of midi input

---
+++ vue/src/courses/piano/utility/SyllableSeqVis.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+
+  <svg :width="510" :height="3 * (high - low) + 10">
+    <template
+      v-for="syl in seq.syllables"
+    >
+      <template
+       v-for="note in syl.notes"
+      >
+        <circle
+          :key="syl.timestamp + '-' + note.note.number" 
+          :cx="( syl.timestamp * 500 / lastTS ) + 4"
+          :cy="3 * (high - note.note.number) + 4"
+          :alt='note.note.name'
+          r="3"
+          :fill="note.isCorrect ? 'black' : 'red'"
+        />
+      </template>
+    </template>
+  </svg>
+</div>
+</template>
+
+
+<script lang="ts">
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+import { log } from 'util';
+import SkldrVue from '../../../SkldrVue';
+import { SyllableSequence } from './midi';
+
+@Component({})
+export default class SyllableSeqVis extends SkldrVue {
+  @Prop()
+  public seq: SyllableSequence;
+
+  public lastTS: number = 0; // in ms
+  public high: number = 0;
+  public low: number = 500;
+
+  created() {
+    try {
+      this.lastTS = this.seq.syllables[this.seq.syllables.length - 1].timestamp
+    } catch { }
+    this.seq.syllables.forEach(s => {
+      s.notes.forEach((n) => {
+        if (n.note.number > this.high) {
+          this.high = n.note.number;
+        }
+        if (n.note.number < this.low) {
+          this.low = n.note.number;
+        }
+      });
+    })
+  }
+}
+</script>
