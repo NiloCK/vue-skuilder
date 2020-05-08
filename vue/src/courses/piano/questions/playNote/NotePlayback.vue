d699bd7793165ccc199bd8faf1a79dcbcd28a7bb
Colin Kennedy <colin@mrkennedy.ca>
2020-05-08 12:34:37 -0600

    Start PlayNote exercise type
    (todo: work on grading - equiv notes / sharps / flats etc)

---
+++ vue/src/courses/piano/questions/playNote/NotePlayback.vue
@@ -0,0 +1,185 @@
+<template>
+  <div>
+    <div class="display-1">Play the note: <span class='font-weight-bold'>{{note}}</span></div>
+
+    <svg 
+      v-if="graded"
+      width="300px"
+      height="auto"
+      xmlns:dc="http://purl.org/dc/elements/1.1/"
+      xmlns:cc="http://creativecommons.org/ns#"
+      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+      xmlns:svg="http://www.w3.org/2000/svg"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+      xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+      sodipodi:docname="keys.svg"
+      inkscape:version="1.0rc1 (09960d6f05, 2020-04-09)"
+      id="svg8" version="1.1" viewBox="0 0 175.24936 73.594925"
+    >
+      <defs id="defs2" />
+      <sodipodi:namedview inkscape:window-maximized="1" inkscape:window-y="726" inkscape:window-x="1491" inkscape:window-height="1321" inkscape:window-width="2400" showborder="false" showgrid="false" inkscape:document-rotation="0" inkscape:current-layer="layer1" inkscape:document-units="mm" inkscape:cy="255.88343" inkscape:cx="754.94284" inkscape:zoom="0.98994949" inkscape:pageshadow="2" inkscape:pageopacity="0.0" borderopacity="1.0" bordercolor="#666666" pagecolor="#ffffff" id="base" />
+      <metadata id="metadata5">
+        <rdf:RDF>
+          <cc:Work rdf:about="">
+            <dc:format>image/svg+xml</dc:format>
+            <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+            <dc:title></dc:title>
+          </cc:Work>
+        </rdf:RDF>
+      </metadata>
+      <g transform="translate(167.75219,-151.96011)" id="layer1" inkscape:groupmode="layer" inkscape:label="Layer 1">
+        <rect y="152.19064" x="-150.12129" height="73.220116" width="17.486626" id="rect845" style="fill:#ffffff;fill-opacity:0;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+        <rect style="fill:#ffffff;fill-opacity:0;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect845-9" width="17.486626" height="73.220116" x="-132.63466" y="152.19064" />
+        <rect style="fill:#ffffff;fill-opacity:0;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect845-6" width="17.486626" height="73.220116" x="-115.14803" y="152.19064" />
+        <rect style="fill:#ffffff;fill-opacity:0;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect845-0" width="17.486626" height="73.220116" x="-45.20153" y="152.19064" />
+        <rect style="fill:#ffffff;fill-opacity:0;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect845-92" width="17.486626" height="73.220116" x="-80.174782" y="152.19064" />
+        <rect style="fill:#ffffff;fill-opacity:0;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect845-3" width="17.486626" height="73.220116" x="-62.688156" y="152.19064" />
+        <rect style="fill:#ffffff;fill-opacity:0;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect845-4" width="17.486626" height="73.220116" x="-97.661407" y="152.19064" />
+        <rect style="fill:#ffffff;fill-opacity:0;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect845-30" width="17.486626" height="73.220116" x="-27.714905" y="152.10745" />
+        <rect ry="0" y="152.10744" x="-138.43532" height="54.415863" width="10.466455" id="rect898" style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+        <rect style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect898-3" width="10.466455" height="54.415863" x="-120.84978" y="152.10744" ry="0" />
+        <rect style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect898-7" width="10.466455" height="54.415863" x="-85.678711" y="152.10744" ry="0" />
+        <rect style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect898-8" width="10.466455" height="54.415863" x="-68.09317" y="152.10744" ry="0" />
+        <rect style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect898-1" width="10.466455" height="54.415863" x="-50.507633" y="152.10744" ry="0" />
+        <rect y="152.10745" x="-10.228278" height="73.220116" width="17.486626" id="rect845-30-1" style="fill:#ffffff;fill-opacity:0;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+        <rect y="152.10744" x="-167.60791" height="73.220116" width="17.486626" id="rect845-30-9" style="fill:#ffffff;fill-opacity:0;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+        <rect style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect898-4" width="10.466455" height="54.415863" x="-15.336554" y="152.10744" ry="0" />
+        <rect style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect898-74" width="4.4679713" height="54.415863" x="-167.60791" y="152.10744" ry="0" />
+        <rect style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.79375;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect898-10" width="5.1863246" height="54.507248" x="2.2092075" y="152.06175" ry="0" />
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import { QuestionView } from '@/base-course/Viewable';
+import SkMidi, { NoteEvent, eventsToSyllableSequence, SyllableSequence } from '../../utility/midi';
+import { PlayNote } from '.';
+import moment from 'moment';
+import SyllableSeqVis from '../../utility/SyllableSeqVis.vue';
+
+@Component({
+  components: {
+    SyllableSeqVis
+  }
+})
+export default class NotePlayback extends QuestionView<PlayNote> {
+
+
+  public midi: SkMidi;
+  public initialized: boolean = false;
+  public playbackInitTime: number;
+  public recording: boolean = false;
+
+  private intervalID: number;
+
+  public playbackProgress: number = 0;
+  public playbackStartTime: moment.Moment = moment.utc();
+  public playbackDuration: number;
+  public attempts: number = 0;
+
+  public gradedSeq: SyllableSequence = eventsToSyllableSequence([]);
+  public graded: boolean = false;
+
+  public inputSeq: SyllableSequence = eventsToSyllableSequence([]);
+
+  public note: string = "";
+
+  public $refs: {
+    progressBar: HTMLDivElement;
+    inputVis: SyllableSeqVis;
+  }
+
+  async created() {
+    this.midi = await SkMidi.instance();
+    this.note = this.question.note;
+  }
+
+  public async mounted() {
+    await SkMidi.instance();
+    this.play();
+
+    this.initialized = true;
+  }
+
+  public record() {
+    this.midi.record();
+    this.recording = true;
+
+    // attach listeners
+    this.midi.addNoteonListenter((e) => {
+      this.inputSeq.append(e);
+    });
+    this.midi.addNoteoffListenter((e) => {
+      if (this.midi.recording.length >= 2) {
+        this.submit();
+      }
+    });
+  }
+
+  public play() {
+    this.midi.stopRecording();
+    this.midi.eraseRecording();
+    this.playbackStartTime = moment.utc();
+    this.recording = false;
+    this.playbackProgress = 0;
+
+    this.record();
+  }
+
+  get question() {
+    return new PlayNote(this.data);
+  }
+
+  public submit() {
+    const aSylSeq = eventsToSyllableSequence(this.midi.recording);
+
+    this.inputSeq = eventsToSyllableSequence([]);
+    // this.$set()
+    // this.gradedSeq = Object.assign({}, this.gradedSeq, {});
+
+    // this.question.isCorrect(this.midi.recording);
+    if (!this.submitAnswer(this.midi.recording).isCorrect) {
+      this.attempts++;
+      this.graded = true;
+      if (this.attempts < this.maxAttemptsPerView) {
+        this.play();
+      }
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+</script>
+
+<style lang="css" >
+@keyframes progress {
+  0% {
+    width: 0%;
+  }
+
+  100% {
+    width: 100%;
+  }
+}
+
+.progressContainer {
+  background-color: #eee;
+  height: 5px;
+}
+
+#progress {
+  background-color: blue;
+  width: 100%;
+  height: 5px;
+  animation-timing-function: linear;
+}
+
+.hidden {
+  opacity: 0;
+}
+</style>
---
+++ vue/src/courses/piano/questions/playNote/index.ts
@@ -0,0 +1,75 @@
+import { Question } from '@/base-course/Displayable';
+import { DataShape } from '@/base-course/Interfaces/DataShape';
+import { FieldDefinition } from '@/base-course/Interfaces/FieldDefinition';
+import { ViewData } from '@/base-course/Interfaces/ViewData';
+import { DataShapeName } from '@/enums/DataShapeNames';
+import { FieldType } from '@/enums/FieldType';
+import NotePlayback from './NotePlayback.vue';
+import { NoteEvent, eventsToSyllableSequence } from '../../utility/midi';
+import { VueConstructor } from 'vue';
+import Viewable from '@/base-course/Viewable';
+import { log } from 'util';
+
+const fields: FieldDefinition[] = [
+  {
+    name: 'Note',
+    type: FieldType.STRING
+  }
+];
+
+export const inputs: string[] = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "A♯", // sharps
+  "C♯",
+  "D♯",
+  "F♯",
+  "G♯",
+  "D♭", // flats
+  "E♭",
+  "G♭",
+  "A♭",
+  "B♭"
+];
+
+export class PlayNote extends Question {
+  public static dataShapes: DataShape[] = [
+    {
+      fields,
+      name: DataShapeName.PIANO_PlayNote
+    }
+  ];
+
+  public static seedData = inputs.map((n) => {
+    return {
+      "Note": n
+    }
+  });
+
+  public static views = [
+    NotePlayback
+  ] as VueConstructor<Viewable>[];
+
+  public note: string;
+
+  constructor(data: ViewData[]) {
+    super(data);
+    this.note = data[0].Note as any as string;
+  }
+
+  public isCorrect(answer: NoteEvent[]): boolean {
+    return answer[0].note.name === this.note;
+  }
+
+  public dataShapes(): DataShape[] {
+    return PlayNote.dataShapes;
+  }
+  public views() {
+    return PlayNote.views as VueConstructor<Viewable>[];
+  }
+}
