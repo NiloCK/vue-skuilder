97e3f720f90111611060725086f62fa007845013
Colin Kennedy <colin@mrkennedy.ca>
2020-05-14 23:23:09 -0600

    Move HelloWorld.vue contents to Home.vue

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,17 +1,117 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container fluid fill-height >
+      <v-layout row align-center>
+        <v-flex >
+          
+          <h1 :class="{'display-3': $vuetify.breakpoint.xs, 'display-4': $vuetify.breakpoint.smAndUp}">
+            <span class="font-weight-thin">edu</span>
+            <span class="font-weight-bold">Quilt</span>
+          </h1>
+          <div class="display-1">An experiment in community-driven learning and teaching</div>
+          <br><br><br>
+          <div class="headline">(let's get cozy)</div>
+        
+        </v-flex>
+        <!-- <div class="section"></div> -->
+        <!-- <div class="section step1">
+          
+          <ul class="headline">
+            <li>Somebody starts a new Quilt</li>
+            <li>Anybody can add content or make connections between content</li>
+            <li>Suggested content and connections are tested with students.
+              Effective, engaging content shines through. The rest falls out of use.</li>
+          </ul>
+        </div>
+        <div class="section step2">
+          Hi
+        </div>
+         -->
+      </v-layout>
+      
+
+    
+  </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import UserLogin from '../components/UserLogin.vue';
+import { Status } from '@/enums/Status';
 
-@Component({
+
+export default {
+  name: 'eduQuilt',
+  props: {
+    msg: String
+  },
   components: {
-    HelloWorld
+    UserLogin
+  },
+  computed: {
+    'asof': () => {
+
+    }
   }
-})
-export default class Home extends Vue { }
+};
 </script>
+
+<style scoped>
+.section:first {
+  padding: 0px 0px 0px 0px;
+}
+.pitch {
+  padding: 50px 0px 0px 0px;
+}
+.section {
+  width: 100%;
+  transform: skewY(-4deg) !important;
+  padding: 200px 0px;
+  margin: 0px 0px;
+  background-color: transparent;
+}
+.section > * {
+  transform: skewY(4deg);
+}
+
+.step1 {
+  background-color: skyblue;
+}
+
+.step2 {
+  background-color: red;
+}
+
+.eduFade {
+  animation: fadein 2s;
+}
+
+.quiltedFade {
+  /* margin-top: 25px; */
+  /* font-size: 21px; */
+  /* text-align: center; */
+
+  animation: delayfadein 2s ease-in-out;
+}
+
+@keyframes delayfadein {
+  0% {
+    opacity: 0;
+  }
+  33% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
+
+
