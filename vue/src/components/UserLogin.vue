c550a61b4a81133a30fd3968436300cb27b37cf9
Colin Kennedy <colin@mrkennedy.ca>
2020-05-26 09:41:13 -0600

    Formatting

--- vue/src/components/UserLogin.vue
+++ vue/src/components/UserLogin.vue
@@ -1,46 +1,46 @@
 <template>
-    <v-card>
-        <v-card-title
-            class="headline grey lighten-2"
-            primary-title
-        >
-            Log In
-        </v-card-title>
+  <v-card>
+      <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+      >
+          Log In
+      </v-card-title>
 
-        <v-card-text>
-        <v-form
-            onsubmit="return false;"  
-        >
-            <v-text-field
-                autofocus
-                name="name"
-                label="Username"
-                id=""
-                v-model="username"
-                prepend-icon="account_circle"
-            ></v-text-field>
-            <v-text-field
-                prepend-icon="lock"
-                name="name"
-                hover="Show password input"
-                label="Enter your password"
-                hint=""
-                min="0"
-                :append-icon="passwordVisible ? 'visibility_off' : 'visibility'"
-                :append-icon-cb="() => (passwordVisible = !passwordVisible)"
-                :type="passwordVisible ? 'text' : 'password'"
-                v-model="password"
-            ></v-text-field>
-            <v-btn type="submit" :loading="awaitingResponse" @click="login" :color="buttonStatus.color">
-                <v-icon left dark>lock_open</v-icon>
-                Log In
-            </v-btn>
-            <v-btn @click="toggle" flat >
-                Create New Account
-            </v-btn>        
-        </v-form>
-        </v-card-text>
-    </v-card>
+      <v-card-text>
+      <v-form
+          onsubmit="return false;"  
+      >
+          <v-text-field
+              autofocus
+              name="name"
+              label="Username"
+              id=""
+              v-model="username"
+              prepend-icon="account_circle"
+          ></v-text-field>
+          <v-text-field
+              prepend-icon="lock"
+              name="name"
+              hover="Show password input"
+              label="Enter your password"
+              hint=""
+              min="0"
+              :append-icon="passwordVisible ? 'visibility_off' : 'visibility'"
+              :append-icon-cb="() => (passwordVisible = !passwordVisible)"
+              :type="passwordVisible ? 'text' : 'password'"
+              v-model="password"
+          ></v-text-field>
+          <v-btn type="submit" :loading="awaitingResponse" @click="login" :color="buttonStatus.color">
+              <v-icon left dark>lock_open</v-icon>
+              Log In
+          </v-btn>
+          <v-btn @click="toggle" flat >
+              Create New Account
+          </v-btn>        
+      </v-form>
+      </v-card-text>
+  </v-card>
 </template>
 
 <script lang="ts">
