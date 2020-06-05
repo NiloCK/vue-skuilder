40a03c7c24b7cc9d21e42f7601e76ae77879ab9a
Colin Kennedy <colin@mrkennedy.ca>
2020-06-05 16:11:18 -0600

    Add global handler for managing card hotkeys

---
+++ vue/src/SkldrMouseTrap.ts
@@ -0,0 +1,59 @@
+import MouseTrap from 'mousetrap';
+
+export interface HotKey extends HotKeyMetaData {
+  callback: (e: ExtendedKeyboardEvent, combo: String) => any;
+}
+export interface HotKeyMetaData {
+  command: string;
+  hotkey: string | string[];
+}
+
+// from https://github.com/ccampbell/mousetrap/tree/master/plugins/global-bind
+(function (a) { var c = {}, d = a.prototype.stopCallback; a.prototype.stopCallback = function (e, b, a, f) { return this.paused ? !0 : c[a] || c[f] ? !1 : d.call(this, e, b, a) }; a.prototype.bindGlobal = function (a, b, d) { this.bind(a, b, d); if (a instanceof Array) for (b = 0; b < a.length; b++)c[a[b]] = !0; else c[a] = !0 }; a.init() })(Mousetrap);
+
+export default class SkldrMouseTrap {
+  private static _instance: SkldrMouseTrap;
+
+  private mouseTrap: MousetrapInstance;
+  private hotkeys: HotKey[];
+
+  private constructor() {
+    this.mouseTrap = new MouseTrap();
+  }
+
+  public static get commands(): HotKeyMetaData[] {
+    return SkldrMouseTrap.instance().hotkeys.map(hk => {
+      return {
+        command: hk.command,
+        hotkey: hk.hotkey
+      }
+    });
+  }
+
+  public static bind(hk: HotKey[]) {
+    SkldrMouseTrap.reset();
+    SkldrMouseTrap.instance().hotkeys = hk;
+
+    hk.forEach(k => {
+      MouseTrap.bindGlobal(k.hotkey, k.callback);
+      // SkldrMouseTrap.instance().mouseTrap.bindGlobal(k.hotkey, k.callback);
+    });
+  }
+
+  public static reset() {
+    MouseTrap.reset();
+    SkldrMouseTrap.instance().mouseTrap.reset();
+  }
+
+  private static instance(): SkldrMouseTrap {
+
+    if (SkldrMouseTrap._instance) {
+      // MouseTrap.bind()
+      return SkldrMouseTrap._instance;
+    } else {
+      SkldrMouseTrap._instance = new SkldrMouseTrap();
+      return SkldrMouseTrap._instance;
+    }
+  }
+
+}
---
+++ vue/src/components/SkMouseTrap.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-dialog
+    max-width="500px"
+    transition="dialog-transition"
+    v-if="display"
+  >
+    
+    <v-btn  fab dark color="primary" slot="activator"  >
+      <v-icon dark>keyboard</v-icon>
+    </v-btn>
+      
+    <v-card>
+      <v-toolbar
+      color="teal"
+      dark
+    >
+      <v-toolbar-title>Shortcut keys for this card:</v-toolbar-title>
+      <v-spacer></v-spacer>
+      
+    </v-toolbar>
+      <v-list>
+        <!-- <v-list-tile>
+          <v-btn outline disabled color='grey' text>Key</v-btn>
+          <v-spacer></v-spacer>
+          <span justify-end>
+            Action
+          </span>
+        </v-list-tile> -->
+        <!-- <v-divider ></v-divider>
+        <v-divider ></v-divider>
+        <v-divider ></v-divider> -->
+        <v-list-tile v-for="hk in commands" :key="hk.hotkey">
+        <v-btn outline color="black">
+          {{hk.hotkey}}
+        </v-btn>
+        <v-spacer></v-spacer>
+        <span justify-end >
+          {{hk.command}}
+        </span>
+          
+        </v-list-tile>
+      </v-list>
+  </v-card>
+    
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import SkldrVue from '../SkldrVue';
+import SkldrMouseTrap, { HotKeyMetaData } from '../SkldrMouseTrap';
+
+@Component({})
+export default class SkldrControlsView extends SkldrVue {
+  public commands: HotKeyMetaData[] = [];
+  public display: boolean = false;
+
+  created() {
+    setInterval(this.refreshState, 500);
+  }
+
+  refreshState() {
+    // console.log(`this.display: ${this.display}`);
+    this.commands = SkldrMouseTrap.commands;
+    this.display = this.commands.length > 0;
+    this.commands = SkldrMouseTrap.commands;
+  }
+
+}
+</script>
