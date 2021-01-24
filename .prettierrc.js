41d9fd6e00a8377cc51a52232337e280e519c837
Andrew Stewart <andrew.gord.stewart@gmail.com>
2021-01-24 16:48:36 -0700

    express: set up eslint/prettier
    following https://dev.to/chgldev/getting-prettier-eslint-and-vscode-to-work-together-3678

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    printWidth: 100,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    overrides: [
+        {
+            files: '*.json',
+            options: {
+                tabWidth: 4,
+            },
+        },
+        {
+            files: '.prettierrc.js',
+            options: {
+                tabWidth: 4,
+            },
+        },
+    ],
+};
