eae9dd5ca54d3c2deb2b2c65a0e04a92f561f67a
Colin Kennedy <colin@mrkennedy.ca>
2021-03-04 23:56:00 -0400

    Prettier Changes

--- vue/src/components/Edit/ViewableDataInputForm/FieldInputs/IntegerInput.vue
+++ vue/src/components/Edit/ViewableDataInputForm/FieldInputs/IntegerInput.vue
@@ -1,30 +1,30 @@
 <template>
-    <v-text-field
-        ref="inputField"
-        box
-        type="number"
-        v-model="store[field.name]"
-        :name="field.name"
-        :label="field.name"
-        @input="validate"
-        :rules="vuetifyRules()"
-    />
+  <v-text-field
+    ref="inputField"
+    box
+    type="number"
+    v-model="store[field.name]"
+    :name="field.name"
+    :label="field.name"
+    @input="validate"
+    :rules="vuetifyRules()"
+  />
 </template>
 
 <script lang="ts">
 import { Component } from 'vue-property-decorator';
 import { integerValidator } from './typeValidators';
-import { FieldInput } from '@/components/Edit/ViewableDataInputForm/FieldInput';
+// import { FieldInput } from '@/components/Edit/ViewableDataInputForm/FieldInput';
 import { log } from 'util';
+import { FieldInput } from '../FieldInput';
 
 @Component({})
 export default class IntegerInput extends FieldInput {
-
-    public get validators() {
-        const ret = super.validators;
-        ret.unshift(integerValidator);
-        log(`validators for ${this.field.name} has ${ret.length} entries`);
-        return ret;
-    }
+  public get validators() {
+    const ret = super.validators;
+    ret.unshift(integerValidator);
+    log(`validators for ${this.field.name} has ${ret.length} entries`);
+    return ret;
+  }
 }
 </script>
