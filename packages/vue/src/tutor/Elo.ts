f63966533ff67b5fbf1086eaf57cfb88810c7290
Colin Kennedy <colin@mrkennedy.ca>
2021-06-23 22:46:37 -0300

    DocComments for adjustScores fcn, small refactor

--- packages/vue/src/tutor/Elo.ts
+++ packages/vue/src/tutor/Elo.ts
@@ -1,8 +1,5 @@
 export class EloRank {
-  k: number;
-  constructor(k?: number) {
-    this.k = k || 32;
-  }
+  constructor(public k: number = 32) {}
 
   setKFactor(k: number) {
     this.k = k;
@@ -19,28 +16,40 @@ export class EloRank {
   }
 }
 
+/**
+ * Calculates updated ELO scores for users and content after they interact
+ *
+ * @param userElo current ELO score of the user
+ * @param cardElo current ELO score of the card
+ * @param userScore user performance against the card in range [0,1]
+ * @param k optional scaling factor. Higher values -> larger score adjustments. Default 32.
+ * @returns
+ */
 export function adjustScores(
   userElo: number,
   cardElo: number,
   userScore: number,
-  k?: number
+  k: number = 32
 ): {
   userElo: number;
   cardElo: number;
 } {
+  if (userScore < 0 || userScore > 1) {
+    throw new Error(`ELO performance rating must be between 0 and 1 - received ${userScore}`);
+  }
   const elo = new EloRank(k);
   const exp = elo.getExpected(userElo, cardElo);
-  const upA = elo.updateRating(exp, userScore, userElo);
-  const upB = elo.updateRating(1 - exp, 1 - userScore, cardElo);
+  const updatedUserElo = elo.updateRating(exp, userScore, userElo);
+  const updatedCardElo = elo.updateRating(1 - exp, 1 - userScore, cardElo);
 
-  console.log(`ELO updates w/ user score ${userScore}
-       user  |  card
-init   ${userElo}         ${cardElo}
-final  ${upA}         ${upB}
-  `);
+  //   console.log(`ELO updates w/ user score ${userScore}
+  //        user  |  card
+  // init   ${userElo}         ${cardElo}
+  // final  ${updatedUserElo}         ${updatedCardElo}
+  //   `);
 
   return {
-    userElo: upA,
-    cardElo: upB,
+    userElo: updatedUserElo,
+    cardElo: updatedCardElo,
   };
 }
