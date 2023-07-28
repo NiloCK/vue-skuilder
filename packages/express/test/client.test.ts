c6ffe7a9072b041077dadbda8fec596ec01b8c36
Colin Kennedy <colin@mrkennedy.ca>
2023-07-28 07:54:50 -0300

    add setup, teardown, & `getVersion` test

---
+++ packages/express/test/client.test.ts
@@ -0,0 +1,30 @@
+import { ChildProcess, exec } from 'child_process';
+import getClient from '../src/client';
+import init from '../src/app';
+import env from '../src/utils/env';
+
+let serverProcess: ChildProcess;
+
+beforeAll(async () => {
+    serverProcess = exec('yarn serve', (err, stdout, stderr) => {});
+
+    // startup time for server.
+    // [ ] replace w/ an actual ready check
+    return await new Promise((resolve) => setTimeout(resolve, 3000));
+})
+
+test('getVersion', async () => {
+    const client = getClient('http://localhost:3000');
+    const version = await client.getVersion();
+    expect(version).toBe(env.VERSION);
+});
+
+afterAll(async () => {
+    // see https://stackoverflow.com/questions/54562879/jest-open-handle-when-using-node-exec/75830272#75830272
+    serverProcess.stdin.destroy();
+    serverProcess.stdout.destroy();
+    serverProcess.stderr.destroy();
+    
+    serverProcess.kill();
+    serverProcess.unref();
+});
