e733b9fb7fd4405b01e58c746bd45f47c1ad0711
Colin Kennedy <colin@mrkennedy.ca>
2023-08-06 21:45:36 -0300

    add create, get, and getClient course methods

--- packages/express/src/client.ts
+++ packages/express/src/client.ts
@@ -1,4 +1,6 @@
-import axios from "axios";
+import axios, { AxiosBasicCredentials, AxiosResponse } from "axios";
+import { CourseConfig, CreateCourse, ServerRequestType } from "../../vue/src/server/types";
+import { CreateCourseResp } from './client-requests/course-requests';
 
 export default class SkldrClient {
     server: string;
@@ -7,9 +9,37 @@ export default class SkldrClient {
         this.server = server;
     }
 
-    // [ ] promisify w/ a check the course exists
-    getCourse(id: string) : SkldrCourse {
-        return {} as SkldrCourse;
+    /**
+     * Create a new course.
+     */
+    async createCourse(cfg: CourseConfig, auth?: AxiosBasicCredentials): Promise<AxiosResponse<CreateCourseResp>> {
+        const request: CreateCourse = {
+            type: ServerRequestType.CREATE_COURSE,
+            data: cfg,
+            user: 'apiClient',
+            response: null,
+        }
+
+        const resp = await axios.post<any, AxiosResponse<CreateCourseResp>>(`${this.server}`, request, {
+            auth: auth,
+        })
+
+        return resp;
+    }
+
+    /**
+     * Creates a client to interact with the specified course.
+     */
+    getCourseClient(id: string): SkldrCourseClient {
+        return new SkldrCourseClient(this.server, id);
+    }
+
+    /**
+     * @returns a list of all courses on the server in `id - name` format.
+     */
+    async getCourses(): Promise<string[]> {
+        const resp = await axios.get(`${this.server}/courses`);
+        return resp.data;
     }
     async getVersion(): Promise<string> {
         const resp = await axios.get(`${this.server}/version`);
