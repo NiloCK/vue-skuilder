b61fe8e6719e077d500997168f60ce3fb177d065
Colin Kennedy <colin@mrkennedy.ca>
2023-08-06 21:50:58 -0300

    add CRuD test for Courses...
    test:
    - creates a course
    - ensures the created course is retrievable
    - deletes the course
    - ensures it is no longer present

--- packages/express/test/client.test.ts
+++ packages/express/test/client.test.ts
@@ -1,9 +1,14 @@
 import { ChildProcess, exec } from 'child_process';
+import { CourseConfig } from '../../vue/src/server/types';
 import Client from '../src/client';
 import env from '../src/utils/env';
 
 let serverProcess: ChildProcess;
 const client: Client = new Client('http://localhost:3000');
+const credentials = {
+    username: env.COUCHDB_ADMIN,
+    password: env.COUCHDB_PASSWORD,
+}
 
 beforeAll(async () => {
     serverProcess = exec('yarn serve', (err, stdout, stderr) => {});
@@ -16,6 +21,46 @@ beforeAll(async () => {
 test('getVersion', async () => {
     const version = await client.getVersion();
     expect(version).toBe(env.VERSION);
+});
+
+test('Course Methods', async () => {
+    const salt = Math.random();
+    const name = 'test-course-' + salt;
+
+    const testCourse: CourseConfig = {
+        name,
+        description: 'test-course',
+        admins: ['test-admin'],
+        creator: 'test-creator',
+        dataShapes: [],
+        questionTypes: [],
+        deleted: false,
+        moderators: [],
+        public: true,
+    }
+
+    // assert non-existence of test course
+    const courses = await client.getCourses();
+    expect( courses.find((c) => c.split(' - ')[1] === testCourse.name) ).toBeUndefined();
+
+    // create test course
+    const createResp = await client.createCourse(testCourse, credentials);
+    expect(createResp.data?.ok).toBe(true);
+    expect(createResp.data?.courseID).not.toBe('');
+    const courseID = createResp.data!.courseID;
+
+    // assert existence of test course
+    const courses2 = await client.getCourses();
+    expect( courses2.find((c) => c.split(' - ')[1] === testCourse.name) ).toBeDefined();
+    expect( courses2.find((c) => c.split(' - ')[0] === courseID) ).toBeDefined();
+    
+    // delete test course
+    const crsClient = client.getCourseClient(courseID);
+    await crsClient.deleteCourse(credentials);
+
+    // assert non-existence of test course
+    const courses3 = await client.getCourses();
+    expect( courses3.find((c) => c.split(' - ')[0] === courseID) ).toBeUndefined();
 });
 
 afterAll(async () => {
