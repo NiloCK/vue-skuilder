ece438119405446be400f600b939964fa0e28a83
Colin Kennedy <colin@mrkennedy.ca>
2024-08-05 22:04:39 -0300

    Add `log` and `error` global helpers

--- packages/vue/src/SkldrVue.ts
+++ packages/vue/src/SkldrVue.ts
@@ -4,4 +4,22 @@ import { AppState } from '@/store';
 
 export default class SkldrVue extends Vue {
   public $store: Store<AppState>;
+
+  /**
+   * Print a message to the console. Prefixes the message with the component
+   * name.
+   * @param message
+   */
+  protected log(message: string): void {
+    console.log(`[SK.${this.$options.name}]: ${message}`);
+  }
+
+  /**
+   * Print an error message to the console. Prefixes the message with the
+   * component name.
+   * @param message 
+   */
+  protected error(message: string): void {
+    console.error(`[SK.${this.$options.name}]: ${message}`);
+  }
 }
