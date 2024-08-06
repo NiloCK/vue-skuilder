ab9f763d4d38c0913d80fb3714d60106a08fc2be
Colin Kennedy <colin@mrkennedy.ca>
2024-08-05 22:07:16 -0300

    Add global `user()` helper

--- packages/vue/src/SkldrVue.ts
+++ packages/vue/src/SkldrVue.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import { Store } from 'vuex';
 import { AppState } from '@/store';
+import { User } from './db/userDB';
 
 export default class SkldrVue extends Vue {
   public $store: Store<AppState>;
@@ -21,5 +22,17 @@ export default class SkldrVue extends Vue {
    */
   protected error(message: string): void {
     console.error(`[SK.${this.$options.name}]: ${message}`);
+  }
+
+  /**
+   * Get the current user from the Vuex store. Throws an error if the user is
+   * not logged in
+   */
+  protected user(): User {
+    if (!this.$store.state._user) {
+      throw new Error('User not logged in');
+    } else {
+      return this.$store.state._user;
+    }
   }
 }
