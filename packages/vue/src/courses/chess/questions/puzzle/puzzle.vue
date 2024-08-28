6c0bcb7d5563f8dfe9b47c4d9da6dc05686b5b79
Colin Kennedy <colin@mrkennedy.ca>
2024-08-27 23:21:10 -0300

    Update puzzle view w/ working chessground, chess.js import...
    currently "plays a game"

--- packages/vue/src/courses/chess/questions/puzzle/puzzle.vue
+++ packages/vue/src/courses/chess/questions/puzzle/puzzle.vue
@@ -1,46 +1,21 @@
 <template>
-  <div width="500px">
-    <!-- <chessboard :fen="question.fen" />
-    <div id="chessground" width="500px"></div> -->
+  <div >
     <div id="cg"></div>
-    {{ question.fen }}
-    <!-- <input type="number" v-model="answer" @keyup.enter="submitAnswer(answer)" /> -->
-    <!-- <UserInputNumber /> -->
-    <div id="cbjs" style="width: 500px"></div>
+    {{ question.fen }}   
   </div>
 </template>
 
 <script lang="ts">
 import { Component } from 'vue-property-decorator';
-// import { chessboard } from 'vue-chessboard';
-// import { Chessground } from 'chessground';
-// import ChessBoard from 'chessboardjs-vue';
+import { Chessground } from '../../chessground/chessground';
+import { Api as cgAPI } from '../../chessground/api';
 import { QuestionView } from '@/base-course/Viewable';
 import { Puzzle } from './index';
-// @ts-ignore
-import cb from 'chessboardjs-vue';
-import Chess from 'chess.js';
-
-// console.log(c.WHITE);
-
-let chess = new Chess.Chess();
-function onDrop(source: any, target: any) {
-  // see if the move is legal
-  var move = chess.move({
-    from: source,
-    to: target,
-    promotion: 'q', // NOTE: always promote to a queen for example simplicity
-  });
-
-  // illegal move
-  if (move === null) return 'snapback';
-
-  // updateStatus();
-}
+import { Chess, SQUARES } from 'chess.js';
 
 @Component({
   components: {
-    cb,
+    // cb,
   },
 })
 export default class PuzzleView extends QuestionView<Puzzle> {
@@ -49,25 +24,76 @@ export default class PuzzleView extends QuestionView<Puzzle> {
     return new Puzzle(this.data);
   }
 
-  // somefen: rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2
-
   public mounted() {
-    // const cg = Chessground(document.getElementById('chessground')!, {
-    //   fen: this.question.fen,
-    // });
-    // const cg = Chessground(document.getElementById('cg')!, {
-    //   fen: this.question.fen,
-    // });
-    const cbjs = cb('cbjs', {
-      draggable: true,
-      position: this.question.fen,
-      // onDrop,
+    let chess = new Chess();
+
+    // this.$nextTick(() => {
+    const cg = Chessground(document.getElementById('cg')!, {
+      movable: {
+        free: false,
+        color: 'both',
+        showDests: true,
+        dests: toDests(chess),
+      },
+      draggable: {
+        showGhost: true,
+      },
+      // fen: this.question.fen,
     });
+
+    cg.set({
+      movable: {
+        events: {
+          after: playOtherSide(cg, chess),
+        },
+      },
+    });
+    // });
   }
 
   public submit() {
     alert(this.question.isCorrect(parseInt(this.answer, 10)));
   }
 }
+
+function toDests(chess: Chess) {
+  const dests = new Map();
+  SQUARES.forEach((s) => {
+    const ms = chess.moves({ square: s, verbose: true });
+    if (ms.length)
+      dests.set(
+        s,
+        ms.map((m) => m.to)
+      );
+  });
+  console.log(dests);
+  return dests;
+}
+
+function toColor(chess: Chess) {
+  return chess.turn() === 'w' ? 'cg-white' : 'cg-black';
+}
+
+function playOtherSide(cg: cgAPI, chess: Chess) {
+  return (orig: any, dest: any) => {
+    chess.move({ from: orig, to: dest });
+    cg.set({
+      turnColor: toColor(chess),
+      movable: {
+        color: toColor(chess),
+        dests: toDests(chess),
+      },
+    });
+  };
+}
 </script>
 
+<style scoped>
+@import '../../chessground/css/chessground.base.css';
+@import '../../chessground/css/chessground.brown.css';
+@import '../../chessground/css/chessground.cburnett.css';
+#cg {
+  width: 400px;
+  height: 400px;
+}
+</style>
