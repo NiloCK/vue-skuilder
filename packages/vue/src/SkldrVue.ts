d6e071780239d410e1c9bc17144529ce86a60ec0
Colin Kennedy <colin@mrkennedy.ca>
2024-09-14 00:01:22 -0300

    add a `warn` logger

--- packages/vue/src/SkldrVue.ts
+++ packages/vue/src/SkldrVue.ts
@@ -24,6 +24,15 @@ export default class SkldrVue extends Vue {
   }
 
   /**
+   * Print a warning message to the console. Prefixes the message with the
+   * component name.
+   * @param message 
+   */
+  protected warn(message?: any, ...optionalParams: any[]): void {
+    console.warn(`[SK.${this.$options.name}]: `, message, ...optionalParams);
+  }
+
+  /**
    * Get the current user from the Vuex store. Throws an error if the user is
    * not logged in
    */
