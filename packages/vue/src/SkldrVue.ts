023c46f85cc861f559839dc5dd50493310c38132
Colin Kennedy <colin@mrkennedy.ca>
2024-09-23 23:01:05 -0300

    use relative import

--- packages/vue/src/SkldrVue.ts
+++ packages/vue/src/SkldrVue.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import { Store } from 'vuex';
-import { AppState } from '@/store';
+import { AppState } from './store';
 import { User } from './db/userDB';
 
 export default class SkldrVue extends Vue {
