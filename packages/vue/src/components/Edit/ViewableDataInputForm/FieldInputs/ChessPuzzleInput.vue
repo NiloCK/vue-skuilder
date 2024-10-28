3088b1f926763f8699332bd29c3cf9894653198f
Colin Kennedy <colin@mrkennedy.ca>
2024-10-27 23:51:36 -0300

    Add chessPuzzleInput type with...
    - `generateTags` method - scrape Lichess raw
    - `generateElo` - similar, unused

---
+++ packages/vue/src/components/Edit/ViewableDataInputForm/FieldInputs/ChessPuzzleInput.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-text-field
+    ref="inputField"
+    box
+    v-model="store[field.name]"
+    v-bind:name="field.name"
+    v-bind:label="field.name"
+    v-bind:rules="vuetifyRules()"
+    v-bind:autofocus="autofocus"
+    v-on:input="validate"
+  />
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator';
+import { FieldInput } from '../FieldInput';
+import { toCourseElo, CourseElo } from '@/tutor/Elo';
+
+@Component
+export default class ChessPuzzleInput extends FieldInput {
+  /*
+  From https://database.lichess.org/#puzzles
+
+  Puzzles are formatted as standard CSV. The fields are as follows:
+
+  PuzzleId,FEN,Moves,Rating,RatingDeviation,Popularity,NbPlays,Themes,GameUrl,OpeningTags
+
+  */
+
+  public mounted() {
+    this.validate();
+  }
+
+  // [ ] move this data e2e - allow to set elo of new cards / notes
+  public generateELO = () => {
+    const split = (this.store[this.field.name] as string).split(',');
+    const elo = parseInt(split[3]);
+    const count = parseInt(split[6]);
+
+    let crsElo: CourseElo = {
+      global: {
+        score: elo,
+        count,
+      },
+      tags: {},
+      misc: {},
+    };
+
+    const tags = this.generateTags();
+    tags.forEach(t => {
+      crsElo.tags[t] = {
+        score: elo,
+        count,
+      };
+    });
+
+    return crsElo;
+  };
+
+  public generateTags = () => {
+    const split = (this.store[this.field.name] as string).split(',');
+    const themes = split[7].split(' ');
+    const openingTags = split[9].split(' ');
+
+    return themes.map(t => `theme-${t}`).concat(openingTags.map(t => `opening-${t}`));
+  };
+}
+</script>
