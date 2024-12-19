cc1298b1e85a53d7186b1056a5f0a63cdd8d1b8e
Colin Kennedy <colin@mrkennedy.ca>
2024-12-19 00:28:03 -0400

    cypress tweaks

--- packages/vue/cypress.config.js
+++ packages/vue/cypress.config.js
@@ -2,8 +2,13 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:8080',
     specPattern: 'tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'tests/cypress/support/e2e.{js,jsx,ts,tsx}',
     fixturesFolder: 'tests/cypress/fixtures',
+    devServer: {
+      command: 'vue-cli-service serve --host localhost',
+      url: 'http://localhost:8080',
+    },
   },
 });
--- packages/vue/tests/cypress/e2e/app.cy.js
+++ packages/vue/tests/cypress/e2e/app.cy.js
@@ -1,5 +1,13 @@
 describe('App Initialization', () => {
   it('renders main app components', () => {
+    cy.log('Environment check');
+    cy.window().then(win => {
+      cy.log('ENV vars:', {
+        COUCHDB_SERVER: win.ENV?.COUCHDB_SERVER_URL,
+        COUCHDB_PROTOCOL: win.ENV?.COUCHDB_SERVER_PROTOCOL,
+      });
+    });
+
     cy.visit('/');
 
     // Wait for app to initialize
