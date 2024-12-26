6628d49d2586a310a6b5ad46930a2d0736d61b92
NiloCK <colin@mrkennedy.ca>
2024-12-26 17:43:11 -0400

    add debug prints to test setup

--- packages/express/test/client.test.ts
+++ packages/express/test/client.test.ts
@@ -2,106 +2,130 @@ import { ChildProcess, exec } from 'child_process';
 import { CourseConfig } from '../../vue/src/server/types';
 import Client from '../src/client';
 import env from '../src/utils/env';
+import { DataShapeName } from '../../vue/src/enums/DataShapeNames';
+import { abort } from 'process';
 
 let serverProcess: ChildProcess;
 const client: Client = new Client('http://localhost:3000');
 const credentials = {
-    username: env.COUCHDB_ADMIN,
-    password: env.COUCHDB_PASSWORD,
-}
+  username: env.COUCHDB_ADMIN,
+  password: env.COUCHDB_PASSWORD,
+};
 
 beforeAll(async () => {
-    serverProcess = exec('yarn serve', (err, stdout, stderr) => { });
-    console.log('Server starting...');
+  serverProcess = exec('yarn serve');
+  serverProcess.stdout.on('data', (data) => {
+    console.log(data);
+  });
+  console.log('Server starting...');
 
-    // startup time for server.
-    // [ ] replace w/ an actual ready check
-    return await new Promise((resolve) => setTimeout(resolve, 7000));
-}, 10_000)
+  const checkServerReady = async () => {
+    return new Promise((resolve, reject) => {
+      const timeout = setTimeout(() => {
+        reject(new Error('Server did not start within 30 seconds'));
+      }, 10000);
+
+      const interval = setInterval(async () => {
+        try {
+          const response = await client.getVersion();
+          if (response) {
+            clearTimeout(timeout);
+            clearInterval(interval);
+            resolve(true);
+          }
+        } catch (error) {
+          // Server not ready yet
+          console.log('Server not ready yet');
+        }
+      }, 1000);
+    });
+  };
+
+  return await checkServerReady();
+}, 10_000);
 
 test('getVersion', async () => {
-    const version = await client.getVersion();
-    expect(version).toBe(env.VERSION);
+  const version = await client.getVersion();
+  expect(version).toBe(env.VERSION);
 });
 
 test('Course Methods', async () => {
-    const testCourse: CourseConfig = createTestCourse();
+  const testCourse: CourseConfig = createTestCourse();
 
-    // assert non-existence of test course
-    const courses = await client.getCourses();
-    expect(courses.find((c) => c.split(' - ')[1] === testCourse.name)).toBeUndefined();
+  // assert non-existence of test course
+  const courses = await client.getCourses();
+  expect(courses.find((c) => c.split(' - ')[1] === testCourse.name)).toBeUndefined();
 
-    // create test course
-    const createResp = await client.createCourse(testCourse, credentials);
-    expect(createResp.data?.ok).toBe(true);
-    expect(createResp.data?.courseID).not.toBe('');
-    const courseID = createResp.data!.courseID;
+  // create test course
+  const createResp = await client.createCourse(testCourse, credentials);
+  expect(createResp.data?.ok).toBe(true);
+  expect(createResp.data?.courseID).not.toBe('');
+  const courseID = createResp.data!.courseID;
 
-    // assert existence of test course
-    const courses2 = await client.getCourses();
-    expect(courses2.find((c) => c.split(' - ')[1] === testCourse.name)).toBeDefined();
-    expect(courses2.find((c) => c.split(' - ')[0] === courseID)).toBeDefined();
+  // assert existence of test course
+  const courses2 = await client.getCourses();
+  expect(courses2.find((c) => c.split(' - ')[1] === testCourse.name)).toBeDefined();
+  expect(courses2.find((c) => c.split(' - ')[0] === courseID)).toBeDefined();
 
-    // delete test course
-    const crsClient = client.getCourseClient(courseID);
-    await crsClient.deleteCourse(credentials);
+  // delete test course
+  const crsClient = client.getCourseClient(courseID);
+  await crsClient.deleteCourse(credentials);
 
-    // assert non-existence of test course
-    const courses3 = await client.getCourses();
-    expect(courses3.find((c) => c.split(' - ')[0] === courseID)).toBeUndefined();
+  // assert non-existence of test course
+  const courses3 = await client.getCourses();
+  expect(courses3.find((c) => c.split(' - ')[0] === courseID)).toBeUndefined();
 });
 
 test('createNote', async () => {
-    const crs = createTestCourse();
-    const createResp = await client.createCourse(crs, credentials);
+  const crs = createTestCourse();
+  const createResp = await client.createCourse(crs, credentials);
 
-    const crsClient = client.getCourseClient(createResp.data!.courseID);
-    const cfg = await crsClient.getConfig();
+  const crsClient = client.getCourseClient(createResp.data!.courseID);
+  const cfg = await crsClient.getConfig();
 
-    for (const k in crs) {
-        expect(cfg[k]).toEqual(crs[k]);
-    }
-    // expect(cfg).toEqual(crs);
-    crsClient.addData({
-        author: 'test-author',
-        data: "this is a test",
-        tags: ['test-tag'],
-        courseID: crsClient.id,
-        codeCourse: 'test-code-course-id',
-        shape: {
-            fields: [],
-            name: DataShapeName.Blanks,
-        },
-    })
+  for (const k in crs) {
+    expect(cfg[k]).toEqual(crs[k]);
+  }
+  // expect(cfg).toEqual(crs);
+  crsClient.addData({
+    author: 'test-author',
+    data: 'this is a test',
+    tags: ['test-tag'],
+    courseID: crsClient.id,
+    codeCourse: 'test-code-course-id',
+    shape: {
+      fields: [],
+      name: DataShapeName.Blanks,
+    },
+  });
 });
 afterAll(async () => {
-    // see https://stackoverflow.com/questions/54562879/jest-open-handle-when-using-node-exec/75830272#75830272
-    serverProcess.stdin.destroy();
-    serverProcess.stdout.destroy();
-    serverProcess.stderr.destroy();
+  // see https://stackoverflow.com/questions/54562879/jest-open-handle-when-using-node-exec/75830272#75830272
+  serverProcess.stdin.destroy();
+  serverProcess.stdout.destroy();
+  serverProcess.stderr.destroy();
 
-    serverProcess.kill();
-    serverProcess.unref();
+  serverProcess.kill();
+  serverProcess.unref();
 });
 
 /**
  * @returns a course config with a randomized name
  */
 function createTestCourse(): CourseConfig {
-    const salt = Math.random();
-    const name = 'test-course-' + salt;
+  const salt = Math.random();
+  const name = 'test-course-' + salt;
 
-    const testCourse: CourseConfig = {
-        name,
-        description: 'test-course',
-        admins: ['test-admin'],
-        creator: 'test-creator',
-        dataShapes: [],
-        questionTypes: [],
-        deleted: false,
-        moderators: [],
-        public: true,
-    };
-    return testCourse;
+  const testCourse: CourseConfig = {
+    name,
+    description: 'test-course',
+    admins: ['test-admin'],
+    creator: 'test-creator',
+    dataShapes: [],
+    questionTypes: [],
+    deleted: false,
+    moderators: [],
+    public: true,
+  };
+  return testCourse;
 }
-
