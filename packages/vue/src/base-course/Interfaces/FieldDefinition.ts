8011484469cd7255a68ef07300a90ab4b085ec12
NiloCK <colin@mrkennedy.ca>
2024-12-26 11:18:24 -0400

    use relative paths...
    required for the hacked build process of `express` app.
    
    express imports from `vue` via relative paths, but does not
    actually build the `vue` project. So child imports must also
    allow for relative pathfinding of deps.

--- packages/vue/src/base-course/Interfaces/FieldDefinition.ts
+++ packages/vue/src/base-course/Interfaces/FieldDefinition.ts
@@ -1,7 +1,7 @@
 import { Validator } from '../../base-course/Interfaces/Validator';
 import { Tagger } from '../../base-course/Interfaces/Tagger';
 import { FieldType } from '../../enums/FieldType';
-import { CourseElo } from '@/tutor/Elo';
+import { CourseElo } from '../../tutor/Elo';
 
 export interface FieldDefinition {
   name: string;
