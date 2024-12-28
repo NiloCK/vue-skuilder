1ddfe7323d28d570210510d7992d279da226147c
NiloCK <colin@mrkennedy.ca>
2024-12-28 16:47:30 -0400

    adjust to daily logs

--- packages/express/src/logger.ts
+++ packages/express/src/logger.ts
@@ -1,12 +1,24 @@
 import { createLogger, format, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
 
 const logger = createLogger({
   level: 'info',
   format: format.combine(format.timestamp(), format.json()),
   transports: [
     new transports.Console(),
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' }),
+    new DailyRotateFile({
+      filename: 'logs/error-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      level: 'error',
+      maxFiles: '7d',
+      maxSize: '20m',
+    }),
+    new DailyRotateFile({
+      filename: 'logs/combined-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      maxFiles: '7d',
+      maxSize: '20m',
+    }),
   ],
 });
 
