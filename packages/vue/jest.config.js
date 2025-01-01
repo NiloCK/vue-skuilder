4033d20ffdb127515604b243019eccbe0e0c69a3
NiloCK <colin@mrkennedy.ca>
2025-01-01 09:00:04 -0400

    update jest dependencies

--- packages/vue/jest.config.js
+++ packages/vue/jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   moduleFileExtensions: ['js', 'jsx', 'json', 'vue', 'ts', 'tsx'],
   transform: {
-    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.vue$': '@vue/vue2-jest',
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
@@ -11,4 +13,14 @@ module.exports = {
   snapshotSerializers: ['jest-serializer-vue'],
   testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
   testURL: 'http://localhost/',
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons'],
+  },
+  transformIgnorePatterns: ['/node_modules/(?!@babel)'],
+  globals: {
+    'vue-jest': {
+      tsConfig: 'tsconfig.json',
+    },
+  },
 };
