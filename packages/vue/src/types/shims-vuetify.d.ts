ae8849bf447a617d59884ee034e198fb5ba522e4
NiloCK <colin@mrkennedy.ca>
2025-01-02 14:34:19 -0400

    add type shim

--- packages/vue/src/types/shims-vuetify.d.ts
+++ packages/vue/src/types/shims-vuetify.d.ts
@@ -3,3 +3,9 @@ declare module 'vuetify/lib' {
   export default Vuetify;
 }
 
+declare module 'vuetify/types' {
+  import { Vue } from 'vue-property-decorator';
+  interface Vue {
+    $vuetify: any;
+  }
+}
