4d954f31d2276776da9cba81097213c7696dc6c0
NiloCK <colin@mrkennedy.ca>
2025-01-04 00:10:30 -0400

    refactor: convert src/components/Edit/ViewableDataInputForm/FieldInputs/StringInput.vue from class-based to Options/Composition API

--- packages/vue/src/components/Edit/ViewableDataInputForm/FieldInputs/StringInput.vue
+++ packages/vue/src/components/Edit/ViewableDataInputForm/FieldInputs/StringInput.vue
@@ -12,9 +12,48 @@
 </template>
 
 <script lang="ts">
-import { Component } from 'vue-property-decorator';
-import { FieldInput } from '../FieldInput';
+import { defineComponent } from 'vue';
+import { SkldrComposable } from '@/mixins/SkldrComposable';
+import type { PropType } from 'vue';
+import type { Field } from '../types'; // Assuming this type exists
 
-@Component
-export default class StringInput extends FieldInput {}
+export default defineComponent({
+  name: 'StringInput',
+  props: {
+    field: {
+      type: Object as PropType<Field>,
+      required: true
+    },
+    store: {
+      type: Object as PropType<Record<string, any>>,
+      required: true
+    },
+    autofocus: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, { emit }) {
+    const { log, warn, error } = SkldrComposable();
+
+    const vuetifyRules = () => {
+      const rules: Array<(v: any) => boolean | string> = [];
+      
+      if (props.field.required) {
+        rules.push((v: any) => !!v || `${props.field.name} is required`);
+      }
+      
+      return rules;
+    };
+
+    const validate = () => {
+      emit('validate');
+    };
+
+    return {
+      vuetifyRules,
+      validate
+    };
+  }
+});
 </script>
