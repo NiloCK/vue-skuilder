ff54174b38be79ab368f651484451d7015f5bd79
NiloCK <colin@mrkennedy.ca>
2025-01-06 13:25:17 -0400

    vuetify 1->2 migration

--- packages/vue/src/components/Courses/CourseRouter.vue
+++ packages/vue/src/components/Courses/CourseRouter.vue
@@ -1,46 +1,50 @@
 <template>
-  <div v-if="initComplete">
-    <course-information v-if="courseId !== undefined && courseId !== ''" v-bind:_id="courseId" />
-    <v-container v-else-if="candidates.length === 0">
-      <v-layout class="display-1" row wrap>
-        {{ query }}
-      </v-layout>
-      <v-divider></v-divider>
-      <v-layout row wrap ma-3 class="headline">Nothing here! </v-layout>
-
-      <v-layout row wrap ma-3>
-        <v-dialog v-model="newCourseDialog" fullscreen transition="dialog-bottom-transition" :overlay="false">
-          <v-btn color="primary" dark slot="activator">Start a new Quilt</v-btn>
-          <course-editor v-bind:name="query" v-on:CourseEditingComplete="newCourseDialog = false" />
-        </v-dialog>
-      </v-layout>
-    </v-container>
-    <v-container v-else grid-list-md>
-      <div>
-        <span class="display-2">{{ query }} </span> <span class="headline">could refer to:</span>
+  <div>
+    <div v-if="initComplete">
+      <course-information v-if="courseId !== undefined && courseId !== ''" v-bind:_id="courseId" />
+      <v-container v-else-if="candidates.length === 0">
+        <v-row class="text-h4">
+          {{ query }}
+        </v-row>
         <v-divider></v-divider>
+        <v-row class="ma-3 text-h5">Nothing here!</v-row>
 
-        <div class="ma-5" v-bind:key="i" v-for="(c, i) in candidates">
-          <v-layout class="headline">
-            <a
-              v-on:click="
-                query = c.courseID;
-                loadQuery();
-              "
-              >{{ c.name }}
-            </a>
-            -
-            <v-text-field
-              label="Disambiguator"
-              vibind:id="`${i}-disambiguator`"
-              v-model="c.disambiguator"
-              v-on:change="update(c)"
-            ></v-text-field>
-            {{ c.description }}
-          </v-layout>
+        <v-row class="ma-3">
+          <v-dialog v-model="newCourseDialog" fullscreen transition="dialog-bottom-transition" :overlay="false">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark v-bind="attrs" v-on="on">Start a new Quilt</v-btn>
+            </template>
+            <course-editor v-bind:name="query" v-on:CourseEditingComplete="newCourseDialog = false" />
+          </v-dialog>
+        </v-row>
+      </v-container>
+      <v-container v-else>
+        <div>
+          <span class="text-h3">{{ query }} </span> <span class="text-h5">could refer to:</span>
+          <v-divider></v-divider>
+
+          <div class="ma-5" v-bind:key="i" v-for="(c, i) in candidates">
+            <v-row class="text-h5">
+              <a
+                v-on:click="
+                  query = c.courseID;
+                  loadQuery();
+                "
+                >{{ c.name }}
+              </a>
+              -
+              <v-text-field
+                label="Disambiguator"
+                :id="`${i}-disambiguator`"
+                v-model="c.disambiguator"
+                v-on:change="update(c)"
+              ></v-text-field>
+              {{ c.description }}
+            </v-row>
+          </div>
         </div>
-      </div>
-    </v-container>
+      </v-container>
+    </div>
   </div>
 </template>
 
@@ -53,7 +57,7 @@ import CourseInformation from './CourseInformation.vue';
 
 export default defineComponent({
   name: 'CourseRouter',
-  
+
   components: {
     CourseInformation,
     CourseEditor,
@@ -62,8 +66,8 @@ export default defineComponent({
   props: {
     query: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
 
   data() {
@@ -72,8 +76,8 @@ export default defineComponent({
       courseId: undefined as string | undefined,
       candidates: [] as CourseConfig[],
       newCourseDialog: false,
-      initComplete: false
-    }
+      initComplete: false,
+    };
   },
 
   methods: {
@@ -90,11 +94,7 @@ export default defineComponent({
 
       this.candidates = this.courseList.filter((c) => {
         const snakedName = c.name.replace(' ', '_').toLowerCase();
-        return (
-          query === snakedName || 
-          query === c.courseID || 
-          query === `${snakedName}_(${c.disambiguator})`
-        );
+        return query === snakedName || query === c.courseID || query === `${snakedName}_(${c.disambiguator})`;
       });
 
       if (this.candidates.length === 1) {
@@ -104,13 +104,13 @@ export default defineComponent({
       }
 
       this.initComplete = true;
-    }
+    },
   },
 
   async created() {
     this.courseList = await getCachedCourseList();
     this.loadQuery();
-  }
+  },
 });
 </script>
 
