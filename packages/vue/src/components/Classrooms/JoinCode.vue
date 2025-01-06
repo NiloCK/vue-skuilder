fe8ced4701f7d88f495181d51e5bf16a2fa6a5d0
NiloCK <colin@mrkennedy.ca>
2025-01-06 11:37:02 -0400

    vuetify 1->2 migration

--- packages/vue/src/components/Classrooms/JoinCode.vue
+++ packages/vue/src/components/Classrooms/JoinCode.vue
@@ -1,16 +1,20 @@
 <template>
-  <v-layout row wrap>
-    <!-- <router-link
-
-  > -->
-    <v-btn fab color="red" right dark absolute @click="close">
-      <v-icon>close</v-icon>
+  <v-container fluid class="fill-height pa-0">
+    <!-- Close button in top-right corner -->
+    <v-btn fab color="red" dark class="close-btn" @click="close">
+      <v-icon>mdi-close</v-icon>
     </v-btn>
-    <!-- </router-link> -->
-    <v-flex pa-5 fill-height text-sm-center class="display-4 justify-height" v-if="!updatePending">
-      {{ _classroomCfg.joinCode }}
-    </v-flex>
-  </v-layout>
+
+    <!-- Main content -->
+    <v-row align="center" justify="center" class="fill-height">
+      <v-col cols="12" class="text-center">
+        <div v-if="!updatePending" class="join-code">
+          {{ _classroomCfg.joinCode }}
+        </div>
+        <v-progress-circular v-else indeterminate size="64"></v-progress-circular>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -21,20 +25,20 @@ import { ClassroomConfig } from '../../server/types';
 
 export default Vue.extend({
   name: 'JoinCode',
-  
+
   props: {
     _id: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
 
   data() {
     return {
       _classroomCfg: null as ClassroomConfig | null,
       classroomDB: null as TeacherClassroomDB | null,
-      updatePending: true
-    }
+      updatePending: true,
+    };
   },
 
   async created() {
@@ -50,7 +54,30 @@ export default Vue.extend({
   methods: {
     close() {
       this.$router.back();
-    }
-  }
+    },
+  },
 });
 </script>
+
+<style>
+.close-btn {
+  position: fixed;
+  top: 80px;
+  right: 16px;
+  z-index: 100;
+}
+
+.join-code {
+  font-size: 8rem;
+  font-weight: bold;
+  line-height: 1.2;
+  letter-spacing: 0.1em;
+}
+
+/* Responsive adjustments */
+@media (max-width: 600px) {
+  .join-code {
+    font-size: 4rem;
+  }
+}
+</style>
