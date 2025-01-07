4ab142929aee3f555bf70e8cc25598d6db4c3c80
NiloCK <colin@mrkennedy.ca>
2025-01-07 11:07:29 -0400

    refactor: convert src/courses/default/questions/basic/BasicCardView.vue from class-based to Options/Composition API
    Summary:
    The conversion process involved:
    1. Replacing the class-based decorator syntax with defineComponent()
    2. Moving the class properties to data()
    3. Converting class methods to the methods object
    4. Converting getters to computed properties
    5. Maintaining the inheritance chain through extends: InformationView
    6. Adding explicit name property
    7. Maintaining TypeScript typing where possible
    
    Warnings:
    1. The generic type parameter <BasicCard> from the original InformationView<BasicCard> class extension is lost in the Options API version. This may impact type safety for operations involving the base class.
    2. The implementation assumes InformationView is compatible with Options API extension. If InformationView contains class-specific features, they might not work as expected.

--- packages/vue/src/courses/default/questions/basic/BasicCardView.vue
+++ packages/vue/src/courses/default/questions/basic/BasicCardView.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { defineComponent } from 'vue';
 import { InformationView } from '@/base-course/Viewable';
 import { Displayable } from '@/base-course/Displayable';
 import { FieldType } from '@/enums/FieldType';
@@ -15,17 +15,27 @@ import { DataShapeName } from '@/enums/DataShapeNames';
 import { log } from 'util';
 import BasicCard from './BasicCard';
 
-// tslint:disable-next-line:max-classes-per-file
-@Component
-export default class BasicView extends InformationView<BasicCard> {
-  public answer: string = '';
+export default defineComponent({
+  name: 'BasicCardView',
+  
+  extends: InformationView,
 
-  public get displayable() {
-    return new BasicCard(this.data);
-  }
+  data() {
+    return {
+      answer: ''
+    };
+  },
 
-  public submit() {
-    log('a');
+  computed: {
+    displayable(): BasicCard {
+      return new BasicCard(this.data);
+    }
+  },
+
+  methods: {
+    submit(): void {
+      log('a');
+    }
   }
-}
+});
 </script>
