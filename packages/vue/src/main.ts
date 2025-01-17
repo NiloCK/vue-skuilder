63700f8da8b8f30a4d730a53594d705d4112fbc2
NiloCK <colin@mrkennedy.ca>
2025-01-16 23:28:23 -0400

    prune old code

--- packages/vue/src/main.ts
+++ packages/vue/src/main.ts
@@ -1,26 +1,10 @@
-// import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import { createApp } from 'vue';
 import App from './App.vue';
-// import vuetify from './plugins/vuetify'; // Only import once
 import './registerServiceWorker';
 import router from './router';
-import { createPinia, PiniaVuePlugin } from 'pinia';
+import { createPinia } from 'pinia';
 import vuetify from './plugins/vuetify';
-
-// Vue.config.productionTip = false;
-
-// Vue.use(PiniaVuePlugin);
-
-// // Ensure Vue uses Vuex before creating the app instance
-// // Vue.use(Vuex);
-
-// new Vue({
-//   router,
-//   pinia,
-//   vuetify,
-//   render: (h) => h(App),
-// }).$mount('#app');
 
 const pinia = createPinia();
 const app = createApp(App);
