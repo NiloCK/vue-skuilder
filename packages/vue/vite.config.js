69a668d35995252abacb81f266ae1f2382f0963e
NiloCK <colin@mrkennedy.ca>
2025-01-17 12:18:30 -0400

    prevent classname mangling

--- packages/vue/vite.config.js
+++ packages/vue/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
 import { VitePWA } from 'vite-plugin-pwa';
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      keep_classnames: true,
+    },
+  },
   define: {
     global: 'window',
     'process.env': process.env,
