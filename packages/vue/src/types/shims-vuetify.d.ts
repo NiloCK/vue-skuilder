c99f382f5d41a7d85d419dc33392e94329e45c79
NiloCK <colin@mrkennedy.ca>
2025-01-19 22:44:59 -0400

    linting

--- packages/vue/src/types/shims-vuetify.d.ts
+++ packages/vue/src/types/shims-vuetify.d.ts
@@ -6,6 +6,7 @@ declare module 'vuetify/lib' {
 declare module 'vuetify/types' {
   import Vue from 'vue';
   interface Vue {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     $vuetify: any;
   }
 }
