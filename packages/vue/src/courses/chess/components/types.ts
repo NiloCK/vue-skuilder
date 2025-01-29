93341008bc279f858cf12ca5de1b342a2101f624
NiloCK <colin@mrkennedy.ca>
2025-01-29 14:02:57 -0400

    remove duplications of CG types

--- packages/vue/src/courses/chess/components/types.ts
+++ packages/vue/src/courses/chess/components/types.ts
@@ -9,19 +9,3 @@ export interface Move {
   to: string;
   promotion?: 'q' | 'r' | 'b' | 'n';
 }
-
-export interface BoardConfig {
-  movable?: {
-    free: boolean;
-    color?: 'white' | 'black' | 'both';
-    showDests?: boolean;
-    dests?: Map<string, string[]>;
-  };
-  viewOnly?: boolean;
-  orientation?: 'white' | 'black';
-  coordinates?: boolean;
-  animation?: {
-    enabled: boolean;
-    duration?: number;
-  };
-}
