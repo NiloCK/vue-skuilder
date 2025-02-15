8fef93ea82779d7665a74b3543bd5ef14ed66547
NiloCK <colin@mrkennedy.ca>
2025-02-15 02:20:52 -0400

    wip: add `common` package

---
+++ packages/common/src/index.ts
@@ -0,0 +1,15 @@
+export { Status } from './enums/Status';
+export type {
+  CreateCourse,
+  CourseConfig,
+  CreateClassroom,
+  JoinClassroom,
+  ClassroomConfig,
+  LeaveClassroom,
+  ServerRequest,
+  ServerRequestType,
+  IServerRequest,
+  IServerResponse,
+} from './wire-format';
+
+export { prepareNote55 } from './db/prepareNote55';
