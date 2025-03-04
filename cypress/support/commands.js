907bd575dce98f9f06f69d8febe3d488ce8a0887
NiloCK <colin@mrkennedy.ca>
2025-03-04 13:52:30 -0400

    [e2e] add a userRegistraion command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -13,3 +13,26 @@
 
 // You can read more about custom commands here:
 // https://on.cypress.io/custom-commands
+
+Cypress.Commands.add('registerUser', (username = null, password = 'securePassword123') => {
+  // Generate a unique username if none provided
+  const finalUsername = username || `testuser${Date.now()}`;
+
+  // Visit the signup page
+  cy.visit('/');
+  cy.contains('Sign Up').click();
+
+  // Fill out the registration form
+  cy.get('input[name="username"]').type(finalUsername);
+  cy.get('input[name="password"]').type(password);
+  cy.get('input[name="retypedPassword"]').type(password);
+
+  // Submit the form
+  cy.contains('button', 'Create Account').click();
+
+  // Wait for registration to complete
+  cy.url().should('include', `/u/${finalUsername}/new`);
+
+  // Return the created username so it can be used in tests
+  return cy.wrap(finalUsername);
+});
