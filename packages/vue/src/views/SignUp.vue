77b3bdc9d16070593ffc199d113ea5f9cd7bcf35
NiloCK <colin@mrkennedy.ca>
2025-03-04 13:50:06 -0400

    do not render signup form if logged in

--- packages/vue/src/views/SignUp.vue
+++ packages/vue/src/views/SignUp.vue
@@ -1,18 +1,27 @@
 <template>
   <v-container class="d-flex align-center maxWidth">
-    <user-registration />
+    <user-registration v-if="!isUserLoggedIn" />
+    <div v-else>Already logged in! Welcome back!</div>
   </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import UserRegistration from '@/components/UserRegistration.vue';
+import { useAuthStore } from '@/stores/useAuthStore';
 
 export default defineComponent({
   name: 'LoginRoute',
 
   components: {
     UserRegistration,
+  },
+
+  computed: {
+    isUserLoggedIn(): boolean {
+      const authStore = useAuthStore();
+      return authStore.isLoggedIn;
+    },
   },
 });
 </script>
